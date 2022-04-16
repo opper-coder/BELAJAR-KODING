06f2235dc0b6c2d99805e9b924c1bff8e989c984
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-04-16 09:32:25 +0800

    Add files via upload

---
+++ KODING/Extensi VS code/Extensi VS code.js
@@ -0,0 +1,19 @@
+tambahka -> menu extensi -> cari dan install
+1. ES7/React/Redux/GraphQl/React-Native snippets-
+   jika belum hafal snippets -> ctrl + shift + p -> es7 -> snippets search -> tampil daftar
+2. ESLint
+    -menstandarkan "" / '' outo, titik koma, otomatis ada maka di setting sebelumnya
+3. Prettier - merapikan otomatis
+4. Bracket Pair Colorizer-
+   mewarnai kurawal, kurung kotak, bulat sesuai warna
+5. Color Hightlight- preview warna di text editor
+6. path intellisense - tinggal tulis ./ sudah ada suggestion
+7. gitLens-saat di hover kita akan lihat siapa yang mengerjakan
+8. code time-caranya ctrl + shift + p: > code time - > dashboard
+angga rizki
+9. js css html formater dari lonefi = Prettier
+10. live server dari ritwich dey = html server hot reload
+
+1. cara menghapus element
+import yang tidak terpakai
+option + shift + o // o atau nol
