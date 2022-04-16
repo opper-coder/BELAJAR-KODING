0b781e8d05438261a29fc02009282e8ba195791e
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-04-16 10:12:08 +0800

    Add files via upload

---
+++ KODING/AGENDA/GMAP/documentasi.js
@@ -0,0 +1,33 @@
+/*
+BELAJAR DARI DOKUMENTASI RESMI GOOGLE PLATFORM
+
+
+
+
+
+*/
+
+/*
+KESIMPULAN SEMENTARA 
+
+1. cari api key
+2. coba embed sederhana
+3. coba jalankan API javascript
+4. coba API JS di REACT
+5. coba API JS di RN
+6. pelajari parameter
+7. pelajari API ANDROID
+8. otak atik API di gredle android studio
+9. cari cara untuk opper data ke android studio di gradle
+
+*/
+
+/*
+CARA MENDAPATKAN API KEY GRATIS:
+*/
+
+- masuk -> login -> https://console.cloud.google.com/getting-started ->
+- 	select a project -> new -> nama project "google API KEY pertama" -> create -> lihat notif selesai atas kanan ->
+  	apis & services di sidebar -> dashboard -> pilih project yang sudah di buat tadi -> tunggu terbuka -> credential ->
+  	create credential -> api key -> dapatkan key api -> AIzaSyACcyWduhvKyytJC_ncywYHzFpYzobpeEk
+
