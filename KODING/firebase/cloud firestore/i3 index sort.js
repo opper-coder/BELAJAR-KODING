a2467058c539f5427325db6e385b8860b740f01f
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-04-30 10:59:09 +0800

    Update i3 index sort.js

--- KODING/firebase/cloud firestore/i3 index sort.js
+++ KODING/firebase/cloud firestore/i3 index sort.js
@@ -1,4 +1,16 @@
-peng INDEX an 
 ---------------------------------------------------------------------------------------
+INDEX 
+- Indeks merupakan faktor penting dalam performa database.
+- index adalah daftar isi, untuk dengan cepat menemukan lokasi item yang diminta.
+- query tanpa index akan lambat jika data besar
+- kecepatan di tentukan hasil temuan bukan besarnya data
+- index ada 2 : 1. kolom tunggal 2. kolom komposit
+- index dapat di buat secara manual atau otomatis
+- index di simpan dalam file dan memiliki biaya tersendiri dalam db firebase
+
 ---------------------------------------------------------------------------------------
+INDEX KOLOM TUNGGAL
+- 
+
+
 ---------------------------------------------------------------------------------------
