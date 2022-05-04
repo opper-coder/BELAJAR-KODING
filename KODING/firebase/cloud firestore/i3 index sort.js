3fe396133d5316b856f0b31dfb88ac87e44bb3f3
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-04 15:39:36 +0800

    Update i3 index sort.js

--- KODING/firebase/cloud firestore/i3 index sort.js
+++ KODING/firebase/cloud firestore/i3 index sort.js
@@ -1,5 +1,11 @@
+INDEX
+=======================================================================================
 ---------------------------------------------------------------------------------------
-INDEX 
+RINGKASAN
+- 
+
+=======================================================================================
+INDEX
 - Indeks merupakan faktor penting dalam performa database.
 - index adalah daftar isi, untuk dengan cepat menemukan lokasi item yang diminta.
 - query tanpa index akan lambat jika data besar
@@ -7,29 +13,30 @@ INDEX
 - index dapat di buat secara manual atau otomatis
 - index di simpan dalam koleksi dan memiliki biaya tersendiri dalam db firebase
 - bentuk penyimpanan adalah: path dan salah satu value kolom
-- bikin konfigurasi kolom dan pengecualian 
+- bikin konfigurasi kolom dan pengecualian
 - index ada 2 : 1. kolom tunggal 2. kolom komposit
-- du index tersebut mengharuskan konfigurrasi 'mode index' dan 'cakupan query'
+- di index tersebut mengharuskan konfigurrasi 'mode index' dan 'cakupan query'
 ---------------------------------------------------------------------------------------
 INDEX OTOMATIS
 - default index untuk kolom disimpan dalam dokumen, sub kolom dalam map
-- setelan default index otomatis membuat index kolom tunggal:
+- setelan default pada 'index otomatis' membuat index 'kolom tunggal':
   
 ---------------------------------------------------------------------------------------
 IGNORE INDEX
-- penghecualian akan berlaku secara inheritance (terwaris)
+- penghecualian akan berlaku secara inheritance (terwaris) terhadap child nya
 - pengecualian hanya berlaku pada index otomatis
 
 ---------------------------------------------------------------------------------------
 INDEX KOLOM TUNGGAL
 - kolom tunggal > di simpan di koleksi > ada dua: asc dsc
 - kolom map > di cakupan koleksi > asc dsc
-- array > array contains > 
+- array > array contains >
 - ????????? belim jelas
 
 ---------------------------------------------------------------------------------------
 INDEX KOMPOSIT
 - index komposit tidak di buat secara otomatis mengingat banyaknya kemungkinan kombinasi query
+  sehingga harus manual
   
 ---------------------------------------------------------------------------------------  
 MODE INDEX
