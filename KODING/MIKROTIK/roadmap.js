64437bf9f08326d0edf7e4e1461a4465b6809c70
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-06 15:59:47 +0800

    Update roadmap.js

--- KODING/MIKROTIK/roadmap.js
+++ KODING/MIKROTIK/roadmap.js
@@ -5,15 +5,41 @@ APLIKASI PROSPEK
 - kalau sudah fix tinggal expansion > maintenance > manual books > pekerja
 - kang klis sudah omzet 80 jt dengan : bella - binohu - saiti - spc - spb - spa - balaan - mantan - pibombo 
   - bolo bongkang - saiti dengan pengguna aktif 400 - 600 aktif pengguna
-- 
+- ==========
 ---------------------------------------------------------------------------------------------------------
 RESOURCE
+- pinjaman
+  - BRI 30.000.000
+  - PAI 10.000.000
 - hardware
   - indiehome
   - mikrotik AH 1100
+  - kabel FO
+  - Tool KIT
+  - hotspot
+  - OLT
   - 
+  - ========
+- Pekerjaan
+  - daftar indiehome
+  - sewa tempat mas rokim
+  - sewa tempat server
+  - ========
 - software
+  - mikrotik
+    - IP alocation
+    - Dasar
+    - VLAN
+    - Mangel(pemecah bandwidth)
+    - mangle private
+    - security
+    - 
+  - mikhmon
+  - mikbotam
+  - hotspot
+  - OLT
 - pasar
+  - 
 ---------------------------------------------------------------------------------------------------------
 HAMBATAN
 ---------------------------------------------------------------------------------------------------------
