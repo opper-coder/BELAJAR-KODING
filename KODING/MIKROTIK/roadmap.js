691b8f94863a14a1e480e459ae7bd196f4daa350
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-06 15:50:01 +0800

    Update roadmap.js

--- KODING/MIKROTIK/roadmap.js
+++ KODING/MIKROTIK/roadmap.js
@@ -1,2 +1,22 @@
 ROADMAP
 =========================================================================================================
+APLIKASI PROSPEK
+- kang muklis terus expansion > pendolo
+- kalau sudah fix tinggal expansion > maintenance > manual books > pekerja
+- kang klis sudah omzet 80 jt dengan : bella - binohu - saiti - spc - spb - spa - balaan - mantan - pibombo 
+  - bolo bongkang - saiti dengan pengguna aktif 400 - 600 aktif pengguna
+- 
+---------------------------------------------------------------------------------------------------------
+RESOURCE
+- hardware
+  - indiehome
+  - mikrotik AH 1100
+  - 
+- software
+- pasar
+---------------------------------------------------------------------------------------------------------
+HAMBATAN
+---------------------------------------------------------------------------------------------------------
+NETWORKING
+---------------------------------------------------------------------------------------------------------
+ROADMAP
