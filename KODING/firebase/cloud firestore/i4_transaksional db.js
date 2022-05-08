21d45edd1e46de510f9cfd9f40812949bcfa51db
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-08 15:47:11 +0800

    Update i4_transaksional db.js

--- KODING/firebase/cloud firestore/i4_transaksional db.js
+++ KODING/firebase/cloud firestore/i4_transaksional db.js
@@ -14,8 +14,20 @@ OPERASI TRANSAKSIONAL
 - di firestore ada 2 jenis
   1. transaksi
   2. batch operasi tulis
-  - 
-  
+
+---------------------------------
+MEMPERBARUI DATA DENGAN TRANSAKSI
+- atomik dapat di gunakan operasi get() lalu diikuti set() update() delete()
+- atomik akan di commit saat semua operasi sudah terkini, jadi saat penulisan 
+  tengah dilakukan lalu salah satu dokumen di update oleh seseorang (pihak lain)
+  maka atomik akan di ulang dari awal lagi, tujuanya agar memastikan data yang di ubah 
+  harus yang terkini dan konsisten
+- syarat :
+  - operasi baca dulu baru boleh tulis, update, delete
+  - Fungsi transaksi seharusnya tidak langsung mengubah status aplikasi.
+  - fungsi yang callback atomik bisa di hit berkali2 saat proses baca atomik terganggu
+  - atomik gagal jika di lakukan saat offline
+- contoh atomik
   
   
   
