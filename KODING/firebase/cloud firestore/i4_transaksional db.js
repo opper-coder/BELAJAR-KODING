96b29ee248999210c6fcfda7336ee3e6cb3ba7cc
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-08 15:10:47 +0800

    Update i4_transaksional db.js

--- KODING/firebase/cloud firestore/i4_transaksional db.js
+++ KODING/firebase/cloud firestore/i4_transaksional db.js
@@ -6,8 +6,30 @@ RINGKASAN
 
 OPERASI TRANSAKSIONAL
 - pada firestore mensukung 'operasi transaksional' atau di sebut 'operasi atomik'
-- adalah sebuah 'rangkaian' operasi baca tulis yang harus sukses semua maka 'ok'
-  atau salah satu gagal maka 'gagal kan semua' 
+- adalah sebuah 'rangkaian' operasi baca tulis (pada beberapa dokumen)
+  yang harus sukses semua maka 'commit'
+  atau salah satu gagal maka 'gagal kan semua'
 - jadi jika ada yg gagal tidak perlu rollback ke masing 'rangkaian'
-- 
+- rangkaian di batasi maksimal 500 dokumen
+- di firestore ada 2 jenis
+  1. transaksi
+  2. batch operasi tulis
+  - 
+  
+  
+  
+  
+  
+  
 -----------------------------------------------------------------------------------------------------------
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
