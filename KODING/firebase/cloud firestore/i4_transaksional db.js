0463a99c9be58dbf370b9fdbbe9ec7e0691f9bef
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-08 14:49:48 +0800

    Create i4_transaksional db.js

---
+++ KODING/firebase/cloud firestore/i4_transaksional db.js
@@ -0,0 +1,13 @@
+TRANSAKSIONAL
+===========================================================================================================
+RINGKASAN
+
+===========================================================================================================
+
+OPERASI TRANSAKSIONAL
+- pada firestore mensukung 'operasi transaksional' atau di sebut 'operasi atomik'
+- adalah sebuah 'rangkaian' operasi baca tulis yang harus sukses semua maka 'ok'
+  atau salah satu gagal maka 'gagal kan semua' 
+- jadi jika ada yg gagal tidak perlu rollback ke masing 'rangkaian'
+- 
+-----------------------------------------------------------------------------------------------------------
