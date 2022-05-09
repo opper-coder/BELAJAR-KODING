3c943015b1e242455f12434642cb25be032d8a54
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-09 20:07:54 +0800

    Update i7 baca data.js

--- KODING/firebase/cloud firestore/i7 baca data.js
+++ KODING/firebase/cloud firestore/i7 baca data.js
@@ -2,3 +2,53 @@
 ================================================================================================
 ------------------------------------------------------------------------------------------------
 BACA DATA SEKALI
+- ini data nya misalnya:
+    import { collection, doc, setDoc } from "firebase/firestore";
+    const citiesRef = collection(db, "cities");
+    await setDoc(doc(citiesRef, "SF"), {
+        name: "San Francisco", state: "CA", country: "USA",
+        capital: false, population: 860000,
+        regions: ["west_coast", "norcal"] });
+    await setDoc(doc(citiesRef, "LA"), {
+        name: "Los Angeles", state: "CA", country: "USA",
+        capital: false, population: 3900000,
+        regions: ["west_coast", "socal"] });
+    await setDoc(doc(citiesRef, "DC"), {
+        name: "Washington, D.C.", state: null, country: "USA",
+        capital: true, population: 680000,
+        regions: ["east_coast"] });
+    await setDoc(doc(citiesRef, "TOK"), {
+        name: "Tokyo", state: null, country: "Japan",
+        capital: true, population: 9000000,
+        regions: ["kanto", "honshu"] });
+    await setDoc(doc(citiesRef, "BJ"), {
+        name: "Beijing", state: null, country: "China",
+        capital: true, population: 21500000,
+        regions: ["jingjinji", "hebei"] });
+
+- Contoh berikut menunjukkan cara mengambil konten dari sebuah dokumen menggunakan get():
+    import { doc, getDoc } from "firebase/firestore";
+    const docRef = doc(db, "cities", "SF");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
