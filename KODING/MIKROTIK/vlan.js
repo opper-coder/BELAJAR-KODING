c9237d0655e1191c2ecfd719ce456534feea146d
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-20 15:12:35 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -3,11 +3,19 @@
 ----------------------------------------------------------------------------------------------------------
 CARA BUAT VLAN
 - interface > tab VLAN > add > 
-  - isikan tab general:
-    - name : "nama" > vlan ID : "number (terserah sbg pengenal )" > 
-      interface pilih interface : "boleh port dan bridge" > 
-  - tab loop :
-    - loop protect : default(opt : on) supaya selalu on saja silahkan
+    - isikan tab general:
+        - name : "nama" > vlan ID : "number (terserah sbg pengenal boleh ngka 10,11,12 dst )" > 
+        interface pilih interface : "pilih port, bridge, vlan" (vlan tidak kayaknya) > 
+    - tab loop :
+        - loop protect : default(opt : on) supaya selalu on saja silahkan
     - tab lainya biarkan
     - kalau mau kasih comment
+    - apply >  OK
 ----------------------------------------------------------------------------------------------------------
+IP ADDRESS
+- setelah interface vLAN di buat lalu buatkan IP ADDRESS ALOKASI untuknya
+  untuk disebar nantinya ke luar sbg anggotanya 
+- caranya:
+    - IP > Adresses > add > 
+        - address : 'alokasi ke bawah' > interace : 'pilih port, bridge, vlan' 
+        
