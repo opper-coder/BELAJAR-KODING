0bebbe53c816acdea5988b7d921b6ac0eed48bea
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-20 14:50:35 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -7,5 +7,7 @@ CARA BUAT VLAN
     - name : "nama" > vlan ID : "number (terserah sbg pengenal )" > 
       interface pilih interface : "boleh port dan bridge" > 
   - tab loop :
-    - loop protect : default(opt : on)
+    - loop protect : default(opt : on) supaya selalu on saja silahkan
+    - tab lainya biarkan
+    - kalau mau kasih comment
 ----------------------------------------------------------------------------------------------------------
