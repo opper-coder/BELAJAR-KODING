aa271f1f5ce80a1f67ac40dbc5840fc4a327b9cd
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-20 15:50:45 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -1,11 +1,9 @@
 ==========================================================================================================
-==========================================================================================================
-----------------------------------------------------------------------------------------------------------
 DESKRIPSI
 - fungsi utama vlan adalah : membawa 2 alamat pada 1 interface(port, bridge) 
-- 
 ----------------------------------------------------------------------------------------------------------
 CARA BUAT VLAN
+- intinya hanya pada 1 langkah ini saja (dalam garis tebal) selebihnya hanya config dasar dibawah 
 - interface > tab VLAN > add > 
     - isikan tab general:
         - name : "nama" > vlan ID : "number (terserah sbg pengenal boleh ngka 10,11,12 dst )" > 
@@ -15,6 +13,7 @@ CARA BUAT VLAN
     - tab lainya biarkan
     - kalau mau kasih comment
     - apply >  OK
+==========================================================================================================
 ----------------------------------------------------------------------------------------------------------
 IP ADDRESS
 - setelah interface vLAN di buat lalu buatkan IP ADDRESS ALOKASI untuknya
@@ -23,13 +22,6 @@ IP ADDRESS
     - IP > Adresses > add > 
         - address : 'alokasi ke bawah' > interace : 'pilih port, bridge, vlan' 
         
-----------------------------------------------------------------------------------------------------------
-DHCP SERVER
-- setiap interface jika mau di bagikan kebawah maka buatkan IP DINAMIC seperti DHCP SERVER
-- caranya:
-    - IP > DHCP Server > add > 
-        DHCP > tombol DHCP Setup > interace : 'pilih port, bridge, vlan' > next2
-    - terpenting perhatikan IP address ya (biasanya sih otomatis)
 ----------------------------------------------------------------------------------------------------------
 CONFIG DASAR PADA INTERFACE
 - jika config dasar sudah di lakukan maka skip langkah ini
@@ -48,7 +40,21 @@ CONFIG DASAR PADA INTERFACE
             tab action >
                 action : masquerade
 ----------------------------------------------------------------------------------------------------------
-KALAU VLAN 
+DHCP SERVER
+- setiap interface jika mau di bagikan kebawah maka buatkan IP DINAMIC seperti DHCP SERVER
+- caranya:
+    - IP > DHCP Server > add > 
+        DHCP > tombol DHCP Setup > interace : 'pilih port, bridge, vlan' > next2
+    - terpenting perhatikan IP address ya (biasanya sih otomatis)
+
+----------------------------------------------------------------------------------------------------------
+TERIMA VLAN
+- cara kirim jaringan 1 port dengan 2 IP sudah di lakukan di atas (kirim VLAN)
+- sekarang bagaimana menerima 1 port dengan 2 IP (terima VLAN)
+- karena yang kita terima 1 port akan di bagi dengan 2 IP maka kita membutuh kan alat SWITCH MANAGEABLE 
+  atau mikrotik yang memiliki chip switch manajebel
+- sekarang kita gunakan microtik saja:
+      - 
 
 
 
