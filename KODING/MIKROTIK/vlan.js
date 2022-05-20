bcb7547eef47db902cdd9b8fe149dcae4b5b458f
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-20 16:37:42 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -55,5 +55,39 @@ TERIMA VLAN
 - karena yang kita terima 1 port akan di bagi dengan 2 IP maka kita membutuh kan alat SWITCH MANAGEABLE 
   atau mikrotik yang memiliki chip switch manajebel
 - sekarang kita gunakan microtik saja:
-      - 
+- pada mikrotik ke 2 ini pastikan sudah di hapus setting defaultnya 
+      - kita hanya mengatur 'bridge' dan 'switch'
+      - karena kita hanya butuhkan sebagai switch maka kita bridge kan dulu 2 port 
+      - lalu BRIDGE : 
+        - interface > BRIDGE > add > interface bridge1 misalnya > port 1 >
+        - buatkan juga untuk bridge 2 dan 3 
+          (karena kita butuhkan 3 port dalam bridge 1 untuk masuk 2 untuk keluar) 
+      - SWITCH:
+        - SWITCH > tab vlan > add
+            - switch : biarkan
+            - VLAN ID : 10 (Sesuaikan dengan VLAN ID pengirim (diatas))
+            - port : ether 1 biarkan sbg trunc lalu tambahkan ether2 dengan klik panah ke bawah (sebagai access)
+                1 port "mode trunc" 2 dan port "mode access"
+            - lalu buatkan juga port access pada port3 seperti port 2 barusan dan vlan ID 20
+        - tab port 
+            - setting port1 > double klik pd ether1 
+                - VLAN mode : secure
+                - VLAN header : add missing
+            - setting port2 > double klik pd ether2
+                - VLAN mode : secure
+                - VLAN header : always strip
+                - VLAN ID : 10 (Sesuaikan dengan VLAN ID pengirim (diatas))  
+            - setting port3 > double klik pd ether2
+                - VLAN mode : secure
+                - VLAN header : always strip
+                - VLAN ID : 20 (Sesuaikan dengan VLAN ID pengirim (diatas))  
+----------------------------------------------------------------------------------------------------------
+- CEK IP
+    - untuk memastikan kita cek IP yang dikirimkan dari server   
+    - cara nya 
+        - sambungkan LAN laptop pd ether yang ingin kita lihat IP nya 
+        - buka terminal :> ipconfir > baca info yang di berikan 
+        - jika sama dengan IP yang dikirimkan sesuai dg ip port vlan maka berhasil
+            
+__________________________________________________________________________________________________________
 
