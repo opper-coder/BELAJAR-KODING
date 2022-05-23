83d24e310283f78262484905bed86d60acfdaa3c
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-23 08:15:47 +0800

    Create queue tree.js

---
+++ KODING/MIKROTIK/queue tree.js
@@ -0,0 +1,11 @@
+===============================================================================================================
+Qtree
+simple queue ataupun qtree adalah implementasi HTB 
+HTB = hirarkikal token bucket adalah limitasi hirarkikal tumpukan
+- berguna 'hirarkikal' untuk divisi2 di kantor dsb jadi bukan hanya limitasi per user tapi juga per grup
+- berguna juga mengatur 'hubungan' parent dan child
+- juga mengatur 'priority' antar child
+- juga mengatur child2 secara 'independen' misalnya queue type nya berbeda dsb
+---------------------------------------------------------------------------------------------------------------
+- 
+_______________________________________________________________________________________________________________
