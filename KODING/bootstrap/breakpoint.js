cd61d026d21dc3ac693910a80002cabd5f3b02b6
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-29 09:45:40 +0800

    Create breakpoint.js

---
+++ KODING/bootstrap/breakpoint.js
@@ -0,0 +1,15 @@
+----------------------------------------------------------------------------------------------------------
+BREAK POINT
+- adalah titik point pada kelebaran layar 
+- modal awal untuk responsif
+- setting pada titik point tersebut untuk maksimal responsif
+- tips: bikin target tampilan misalnya 3 tampilan berbeda 
+  </> sm, md, lg, xl, xxl
+----------------------------------------------------------------------------------------------------------
+CONTAINER
+- wadah utama operasi grid sistem dan layout lainya
+- bisa bersarang tapi sebaiknya tidak
+- ada 3 jenis
+  - container = tempat grid sistem
+  - container-fluid  = full layar bisa grid system
+  - container-sm dst = akan responsif dengan breaaak point
