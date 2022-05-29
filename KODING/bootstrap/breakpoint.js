33b3d4d34cf1a540b2a75db95dacbc79a51172f8
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-29 10:49:22 +0800

    Update breakpoint.js

--- KODING/bootstrap/breakpoint.js
+++ KODING/bootstrap/breakpoint.js
@@ -13,3 +13,12 @@ CONTAINER
   - container = tempat grid sistem
   - container-fluid  = full layar bisa grid system
   - container-sm dst = akan responsif dengan breaaak point
+- jika kita akan membuat layar responsif maka kita gunakan container-<brekapoint> 
+  - artinya konten akan 100% lebarnya memenuhi layar jika kekecilan layar berada di bawah breka point
+  - prakteknya tata jajar ke kanan sampai kekecilan break poin. maka akan ke bawah
+  - </>
+    <div class="container-sm">100% wide until small breakpoint</div>
+    <div class="container-md">100% wide until medium breakpoint</div>
+    <div class="container-lg">100% wide until large breakpoint</div>
+    <div class="container-xl">100% wide until extra large breakpoint</div>
+    <div class="container-xxl">100% wide until extra extra large breakpoint</div>
