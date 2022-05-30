b9cd4302e497c2538aaf410f17385b8efee717d5
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-31 06:34:00 +0800

    Update Utilities.js

--- KODING/bootstrap/Utilities.js
+++ KODING/bootstrap/Utilities.js
@@ -17,8 +17,16 @@ TEXT COLOR
   - text-primary
 -----------------------------------------------------------------------------------------------------------------------
 DISPLAY
+.d-none                     // cara menambahkan display
+none,block,inline,inline-block,grid,table,table-cell,table-row,flex,inline-flex
+.d-md-block                 // display dengan responsive breakpoint
+sm, md, lg, xl, dan xxl.
+.d-none .d-sm-block         // kombinasi: sembunyikan hanya di sm: 
+                            // sembunyikan/tampilkan  hanya di <display tentukan> ada tabel kombinasi di dokumentasi
+.d-print-inline             // bahkan hanya tampilkan sembunyikan saat di printer 
 -----------------------------------------------------------------------------------------------------------------------
 FLEX
+
 -----------------------------------------------------------------------------------------------------------------------
 FLOAT
 -----------------------------------------------------------------------------------------------------------------------
