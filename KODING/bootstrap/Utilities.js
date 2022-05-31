b8ffdcb1816ccb54728e2c3067fe130e06ff374d
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-05-31 17:55:11 +0800

    Update Utilities.js

--- KODING/bootstrap/Utilities.js
+++ KODING/bootstrap/Utilities.js
@@ -26,13 +26,25 @@ sm, md, lg, xl, dan xxl.
 .d-print-inline             // bahkan hanya tampilkan sembunyikan saat di printer 
 -----------------------------------------------------------------------------------------------------------------------
 FLOAT
+.float-start :start,end,none // untuk mendorong elemen ke sisi kanan kiri atau hapus
+.float-sm-start             // responsive
 -----------------------------------------------------------------------------------------------------------------------
-INTERACTION/RELEASE MOUSE
------------------------------------------------------------------------------------------------------------------------
+INTERACTION mouse dan klik
+- saat di klik text
+- oiya ini hanya berlaku pada mause tidak untuk keyboard harus di tangani dengan disabled HTML
+  .user-select-all          // saat paragraf di klik maka akan terseleksi seluruh paragraf
+  .user-select-auto         // saat di klik paragraf maka yang terseleksi 1 kata
+  .user-select-none         // tak bisa di seleksi sedikitpun
+- link saat di klik
+  .pe-none                  // link tidak dapat diklik
+  .pe-auto                  // default nya bisa di klik
+  .pe-none>.pe-auto         // saat container .pe-none maka inheriten pd child kecuali di batalkan oleh .pe-auto 
+----------------------------------------------------------------------------------------------------------------------
 OVERFLOW
   - .overflow-auto = hidden, visible, scroll
 -----------------------------------------------------------------------------------------------------------------------
 POSISION
+
 -----------------------------------------------------------------------------------------------------------------------
 SHADOW
 -----------------------------------------------------------------------------------------------------------------------
