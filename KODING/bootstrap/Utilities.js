6d29cb45df13a791652b7974f39ea7125986a4fe
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-06-05 19:14:49 +0800

    Update Utilities.js

--- KODING/bootstrap/Utilities.js
+++ KODING/bootstrap/Utilities.js
@@ -101,7 +101,13 @@ gap grid
 .gap-3  : 0-5, auto
 -----------------------------------------------------------------------------------------------------------------------
 ICON
-
+BOOTSTRAP
+  1. copas > link CDN icon bootstrap di dalam <head>
+  2. buka icon >  yang di pilih >  pilih <i class="bi bi-zoom-out"></i> paste di tempatnya
+  3. resize dan warna pakai CSS > <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
+GOOGLE MATERIAL ICON
+  1. buka icon material 
+  2. pilih salah satu icon > muncul instruksi > copas CDN di head > copas atribut Di CSS nya > copas insert kode icon
 -----------------------------------------------------------------------------------------------------------------------
 FONT FAMILY
 
