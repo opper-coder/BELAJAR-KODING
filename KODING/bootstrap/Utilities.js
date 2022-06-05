00828e02ee5266e3b2342f6f5220561b7824354a
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-06-05 20:22:57 +0800

    Update Utilities.js

--- KODING/bootstrap/Utilities.js
+++ KODING/bootstrap/Utilities.js
@@ -99,25 +99,4 @@ visibility ini tidak mempengaruhi display dan masih menempati ruangan
 -----------------------------------------------------------------------------------------------------------------------
 gap grid
 .gap-3  : 0-5, auto
------------------------------------------------------------------------------------------------------------------------
-ICON
-BOOTSTRAP
-  1. copas > link CDN icon bootstrap di dalam <head>
-  2. buka icon >  yang di pilih >  pilih <i class="bi bi-zoom-out"></i> paste di tempatnya
-  3. resize dan warna pakai CSS > <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
-GOOGLE MATERIAL ICON
-  1. buka icon material 
-  2. pilih salah satu icon > muncul instruksi > 
-     > copas CDN di head 
-     > copas atribut Di CSS nya 
-     > copas insert kode icon
-  3. contoh : <span class="material-symbols-outlined" style="font-size: 5rem; color:red;">
------------------------------------------------------------------------------------------------------------------------
-FONT FAMILY 
-GOOGLE FONTS
-1. buka= https://fonts.google.com/ > pilih font > select this style > 
-   lihat icon pojok kanan atas (view selected families) 
-   > ada petunjuk embed : link atau @import
-   > kalau saya pilih import dari CSS 
-   > sudah bisa di gunakan di CSS: copas nama font style
 _______________________________________________________________________________________________________________________
