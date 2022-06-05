f43941abd8649696891fe4ac4087956097772afd
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-06-05 20:18:47 +0800

    Update Utilities.js

--- KODING/bootstrap/Utilities.js
+++ KODING/bootstrap/Utilities.js
@@ -107,12 +107,17 @@ BOOTSTRAP
   3. resize dan warna pakai CSS > <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
 GOOGLE MATERIAL ICON
   1. buka icon material 
-  2. pilih salah satu icon > muncul instruksi > copas CDN di head > copas atribut Di CSS nya > copas insert kode icon
+  2. pilih salah satu icon > muncul instruksi > 
+     > copas CDN di head 
+     > copas atribut Di CSS nya 
+     > copas insert kode icon
+  3. contoh : <span class="material-symbols-outlined" style="font-size: 5rem; color:red;">
 -----------------------------------------------------------------------------------------------------------------------
 FONT FAMILY 
 GOOGLE FONTS
 1. buka= https://fonts.google.com/ > pilih font > select this style > 
    lihat icon pojok kanan atas (view selected families) 
-   > ada petunjuk embed : link atau @import . 
-   > kalau saya pilih import dari css > sudah bisa di gunakan di CSS: copas nama font style
+   > ada petunjuk embed : link atau @import
+   > kalau saya pilih import dari CSS 
+   > sudah bisa di gunakan di CSS: copas nama font style
 _______________________________________________________________________________________________________________________
