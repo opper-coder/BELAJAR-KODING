f5fdabd39e10112abd4c4053d0a3be961dc1ac13
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-12 13:12:09 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -1,40 +1,18 @@
-RINGAKASAN
-
-CARA BUAT VLAN
-- interface > tab VLAN > add > 
-    - isikan tab general:
-        - name : "aqil-VLAN-10" > vlan ID : "number (terserah sbg pengenal boleh ngka 10,11,12 dst )" > 
-        interface pilih interface : "pilih port, bridge, vlan" (vlan tidak kayaknya) > 
-    - tab loop :
-        - loop protect : default(opt : on) supaya selalu on saja silahkan
-    - tab lainya biarkan
-    - kalau mau kasih comment
-    - apply >  OK
-==========================================================================================================
-DESKRIPSI
-- fungsi utama vlan adalah : membawa 2> alamat(2ip, 2service) pada 1 interface(port, bridge)
-- intinya hanya pada 1 langkah ini saja (dalam garis tebal) selebihnya hanya config dasar dibawah 
-- vlan bisa di terapkan pada BRIDGE ya
-    - tinggal pilih BRIDGE pada saat bikin interface VLAN
-==========================================================================================================        
+RINGAKASAN        
 ----------------------------------------------------------------------------------------------------------
 CONFIG DASAR PADA INTERFACE
 - jika config dasar sudah di lakukan maka skip langkah ini
 ---------------------------
-- ambil internet dari ISP
-- IP > DHCP client > add > interface ether 1 > centang DNS dan NTP > Apply - OK > bound
+- ISP ,dapatkan internet dari ISP:
+- IP > 
+    1. DHCP client > add > interface ether 1 > centang DNS dan NTP > default route: yes > Apply - OK > bound
+    2. butikan: terminal ping: 8.8.8.8, DNS, ping google.com
 ---------------------------
-IP ADDRESS VLAN
-- setelah interface vLAN di buat lalu buatkan IP ADDRESS ALOKASI untuknya
-  untuk disebar nantinya ke luar sbg anggotanya 
-- caranya:
-    - IP > Adresses > add > 
-        - address : 'alokasi ke bawah/pakai slash 24' > interace : 'pilih port, bridge, vlan' 
----------------------------
+NAT MASQUERADE:
 - agar bisa di bagikan sekaligus bisa akses internet untuk client maka kita kasih config dasar padanya
-    - DNS:
+    1. DNS:
         IP > DNS > centang 'allow remote request'
-    - FIREWALL:
+    2. FIREWALL:
         tab NAT > add
             tab general > 
                 chain : srcnat
@@ -42,14 +20,34 @@ IP ADDRESS VLAN
             tab action >
                 action : masquerade
 ----------------------------------------------------------------------------------------------------------
-DHCP SERVER
-- setiap interface jika mau di bagikan kebawah maka buatkan IP DINAMIC seperti DHCP SERVER
-- caranya:
-    - IP > DHCP Server > add > 
-        DHCP > tombol DHCP Setup > interace : 'pilih port, bridge, vlan' > next2
-    - terpenting perhatikan IP address ya (biasanya sih otomatis)
-
+BUAT VLAN
+- interface > add > VLAN > 
+    1. name : "VLAN-10"                      // nama dan angka sesuaikan ID 
+    2. vlan ID : 10                          // 10 samakan dg nama engkja terserah 10,11,12 atau 100 200 300
+    3. loop protect : default(opt : on)      // supaya selalu on saja silahkan
+    4. interface : ether2                    // pilih interface "pilih port, bridge" jika banyak bikin vlan tumpuk di ether ini saja
+    5. apply > Ok
+        - kalau mau kasih comment            // optional
+==========================================================================================================
+DESKRIPSI
+- fungsi utama vlan adalah : membawa 2> alamat(2ip, 2service) pada 1 interface(port, bridge)
+- intinya hanya pada 1 langkah ini saja (dalam garis tebal) selebihnya hanya config dasar dibawah 
+- vlan bisa di kombinasikan pada BRIDGE kayakknya - tinggal pilih BRIDGE pada saat bikin interface VLAN
+==========================================================================================================
+IP ADDRESS VLAN
+vlan ini dibuat 2 buah yang akan menjadi server yang berbeda ke bawah maka kita harus buatkan IP gateway (pakai slash)
+- tips pakai slash: buat satu IP pada port atau bridge nganggur untuk dihapus lagi nantinya > saat bikin IP pakai slah 22 miasalnya
+- lalu berikutnya buatkan DHCP server pada IP port tersebut > pada saat next2 ada rentang yang di berikan akan terlihat silahkan coba
+pada masing2 vlan 10.10.10.10/22 dan 20.20.20.20/24 misalnya
+    1. IP > Adresses > add > address :10.10.10.10/22  > interace : VLAN-10 // interface dalam kasus ini pilih vlan-10
+    2. bikin IP untuk vlan berikutnya juga
 ----------------------------------------------------------------------------------------------------------
+DHCP SERVER
+- setiap interface jika mau di bagikan kebawah maka buatkan IP server pada Interface bersangkutan (ETHER BRIDGE VLAN)
+  seperti IP DINAMIC DHCP SERVER, IP STATIS, HOTSPOT sesuai kebutuhan, dalam kasus ini dinamis
+    1. IP > DHCP Server > DHCP > tombol DHCP Setup > interace : VLAN-10 > next2 // dalam kasus ini pilih VLAN10 (port, bridge, vlan)
+        - terpenting perhatikan IP address ya (biasanya sih otomatis)
+__________________________________________________________________________________________________________
 TERIMA VLAN
 - cara kirim jaringan 1 port dengan 2 IP sudah di lakukan di atas (kirim VLAN)
 - sekarang bagaimana menerima 1 port dengan 2 IP (terima VLAN)
