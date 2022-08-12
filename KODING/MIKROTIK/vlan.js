2dd2123b1084a66a4202b3b25c61f86f765be016
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-12 13:15:08 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -1,13 +1,14 @@
 RINGAKASAN        
-----------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
 CONFIG DASAR PADA INTERFACE
+--------------------------------------------------------------------------------------
 - jika config dasar sudah di lakukan maka skip langkah ini
----------------------------
+---------
 - ISP ,dapatkan internet dari ISP:
 - IP > 
     1. DHCP client > add > interface ether 1 > centang DNS dan NTP > default route: yes > Apply - OK > bound
     2. butikan: terminal ping: 8.8.8.8, DNS, ping google.com
----------------------------
+---------
 NAT MASQUERADE:
 - agar bisa di bagikan sekaligus bisa akses internet untuk client maka kita kasih config dasar padanya
     1. DNS:
@@ -19,8 +20,9 @@ NAT MASQUERADE:
                 out Interface : 'pilih port, bridge, vlan' 
             tab action >
                 action : masquerade
-----------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
 BUAT VLAN
+--------------------------------------------------------------------------------------
 - interface > add > VLAN > 
     1. name : "VLAN-10"                      // nama dan angka sesuaikan ID 
     2. vlan ID : 10                          // 10 samakan dg nama engkja terserah 10,11,12 atau 100 200 300
@@ -28,12 +30,12 @@ BUAT VLAN
     4. interface : ether2                    // pilih interface "pilih port, bridge" jika banyak bikin vlan tumpuk di ether ini saja
     5. apply > Ok
         - kalau mau kasih comment            // optional
-==========================================================================================================
+======================================================================================
 DESKRIPSI
 - fungsi utama vlan adalah : membawa 2> alamat(2ip, 2service) pada 1 interface(port, bridge)
 - intinya hanya pada 1 langkah ini saja (dalam garis tebal) selebihnya hanya config dasar dibawah 
 - vlan bisa di kombinasikan pada BRIDGE kayakknya - tinggal pilih BRIDGE pada saat bikin interface VLAN
-==========================================================================================================
+======================================================================================
 IP ADDRESS VLAN
 vlan ini dibuat 2 buah yang akan menjadi server yang berbeda ke bawah maka kita harus buatkan IP gateway (pakai slash)
 - tips pakai slash: buat satu IP pada port atau bridge nganggur untuk dihapus lagi nantinya > saat bikin IP pakai slah 22 miasalnya
@@ -41,13 +43,13 @@ vlan ini dibuat 2 buah yang akan menjadi server yang berbeda ke bawah maka kita 
 pada masing2 vlan 10.10.10.10/22 dan 20.20.20.20/24 misalnya
     1. IP > Adresses > add > address :10.10.10.10/22  > interace : VLAN-10 // interface dalam kasus ini pilih vlan-10
     2. bikin IP untuk vlan berikutnya juga
-----------------------------------------------------------------------------------------------------------
+---------
 DHCP SERVER
 - setiap interface jika mau di bagikan kebawah maka buatkan IP server pada Interface bersangkutan (ETHER BRIDGE VLAN)
   seperti IP DINAMIC DHCP SERVER, IP STATIS, HOTSPOT sesuai kebutuhan, dalam kasus ini dinamis
     1. IP > DHCP Server > DHCP > tombol DHCP Setup > interace : VLAN-10 > next2 // dalam kasus ini pilih VLAN10 (port, bridge, vlan)
         - terpenting perhatikan IP address ya (biasanya sih otomatis)
-__________________________________________________________________________________________________________
+______________________________________________________________________________________
 TERIMA VLAN
 - cara kirim jaringan 1 port dengan 2 IP sudah di lakukan di atas (kirim VLAN)
 - sekarang bagaimana menerima 1 port dengan 2 IP (terima VLAN)
@@ -80,7 +82,7 @@ TERIMA VLAN
                 - VLAN mode : secure
                 - VLAN header : always strip
                 - VLAN ID : 20 (Sesuaikan dengan VLAN ID pengirim (diatas))  
-----------------------------------------------------------------------------------------------------------
+---------
 - CEK IP
     - untuk memastikan kita cek IP yang dikirimkan dari server
     - cara nya
@@ -88,5 +90,5 @@ TERIMA VLAN
         - buka terminal :> ipconfir > baca info yang di berikan 
         - jika sama dengan IP yang dikirimkan sesuai dg ip port vlan maka berhasil
             
-__________________________________________________________________________________________________________
+______________________________________________________________________________________
 
