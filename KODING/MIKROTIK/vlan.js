fa52c33b4a5f90e7cb8c089ef5a8ac68e58fd999
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-12 12:20:39 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -3,7 +3,7 @@ RINGAKASAN
 CARA BUAT VLAN
 - interface > tab VLAN > add > 
     - isikan tab general:
-        - name : "nama" > vlan ID : "number (terserah sbg pengenal boleh ngka 10,11,12 dst )" > 
+        - name : "aqil-VLAN-10" > vlan ID : "number (terserah sbg pengenal boleh ngka 10,11,12 dst )" > 
         interface pilih interface : "pilih port, bridge, vlan" (vlan tidak kayaknya) > 
     - tab loop :
         - loop protect : default(opt : on) supaya selalu on saja silahkan
@@ -12,7 +12,7 @@ CARA BUAT VLAN
     - apply >  OK
 ==========================================================================================================
 DESKRIPSI
-- fungsi utama vlan adalah : membawa 2 alamat pada 1 interface(port, bridge)
+- fungsi utama vlan adalah : membawa 2> alamat(2ip, 2service) pada 1 interface(port, bridge)
 - intinya hanya pada 1 langkah ini saja (dalam garis tebal) selebihnya hanya config dasar dibawah 
 - vlan bisa di terapkan pada BRIDGE ya
     - tinggal pilih BRIDGE pada saat bikin interface VLAN
@@ -29,7 +29,7 @@ IP ADDRESS VLAN
   untuk disebar nantinya ke luar sbg anggotanya 
 - caranya:
     - IP > Adresses > add > 
-        - address : 'alokasi ke bawah' > interace : 'pilih port, bridge, vlan' 
+        - address : 'alokasi ke bawah/pakai slash 24' > interace : 'pilih port, bridge, vlan' 
 ---------------------------
 - agar bisa di bagikan sekaligus bisa akses internet untuk client maka kita kasih config dasar padanya
     - DNS:
@@ -59,7 +59,7 @@ TERIMA VLAN
 - pada mikrotik ke 2 ini pastikan sudah di hapus setting defaultnya 
       - kita hanya mengatur 'bridge' dan 'switch'
       - karena kita hanya butuhkan sebagai switch maka kita bridge kan dulu 2 port 
-      - lalu BRIDGE : 
+      - BRIDGE : 
         - interface > BRIDGE > add > interface bridge1 misalnya > port 1 >
         - buatkan juga untuk bridge 2 dan 3 
           (karena kita butuhkan 3 port dalam bridge 1 untuk masuk 2 untuk keluar) 
@@ -70,7 +70,7 @@ TERIMA VLAN
             - port : ether 1 biarkan sbg trunc lalu tambahkan ether2 dengan klik panah ke bawah (sebagai access)
                 1 port "mode trunc" 2 dan port "mode access"
             - lalu buatkan juga port access pada port3 seperti port 2 barusan dan vlan ID 20
-        - tab port 
+        - tab PORT:
             - setting port1 > double klik pd ether1 
                 - VLAN mode : secure
                 - VLAN header : add missing
@@ -84,8 +84,8 @@ TERIMA VLAN
                 - VLAN ID : 20 (Sesuaikan dengan VLAN ID pengirim (diatas))  
 ----------------------------------------------------------------------------------------------------------
 - CEK IP
-    - untuk memastikan kita cek IP yang dikirimkan dari server   
-    - cara nya 
+    - untuk memastikan kita cek IP yang dikirimkan dari server
+    - cara nya
         - sambungkan LAN laptop pd ether yang ingin kita lihat IP nya 
         - buka terminal :> ipconfir > baca info yang di berikan 
         - jika sama dengan IP yang dikirimkan sesuai dg ip port vlan maka berhasil
