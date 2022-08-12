3468f530cf3094ca8707c4186c644820d3093831
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-12 19:03:58 +0800

    Update vlan.js

--- KODING/MIKROTIK/vlan.js
+++ KODING/MIKROTIK/vlan.js
@@ -1,36 +1,29 @@
 RINGAKASAN        
 --------------------------------------------------------------------------------------
 CONFIG DASAR PADA INTERFACE
---------------------------------------------------------------------------------------
-- jika config dasar sudah di lakukan maka skip langkah ini
+jika config dasar sudah di lakukan maka skip langkah ini
 ---------
-- ISP, dapatkan internet dari ISP:
-- IP > 
-    1. DHCP client > add > interface ether 1 > centang DNS dan NTP > default route: yes > Apply - OK > bound
+ISP
+    1. IP > DHCP client > add > interface ether-1 > centang DNS dan NTP > default route: yes > Apply - OK > bound
     2. butikan: terminal ping: 8.8.8.8, DNS, ping google.com
 ---------
-NAT MASQUERADE:
-- agar bisa di bagikan sekaligus bisa akses internet untuk client maka kita kasih config dasar padanya
+DNS dan NAT MASQUERADE:
+agar bisa di bagikan sekaligus bisa akses internet untuk client maka kita kasih config dasar padanya
     1. DNS:
-        IP > DNS > centang 'allow remote request'
+       IP > DNS > centang 'allow remote request'
     2. FIREWALL:
-        tab NAT > add
-            tab general > 
-                chain : srcnat
-                out Interface : 'pilih port, bridge, vlan' 
-            tab action >
-                action : masquerade
+       tab NAT > add > tab general > chain : srcnat > out Interface : 'ISP/atau ether-1' 
+            tab action > action : masquerade
 --------------------------------------------------------------------------------------
 BUAT VLAN
 --------------------------------------------------------------------------------------
 - interface > add > VLAN > 
-    1. name : "VLAN-10"                      // nama dan angka sesuaikan ID 
+    1. name : "VLAN-10"                      // nama dan angka sesuaikan vlan-ID 
     2. vlan ID : 10                          // 10 samakan dg nama engkja terserah 10,11,12 atau 100 200 300
-    3. loop protect : default(opt : on)      // supaya selalu on saja silahkan
-    4. interface : ether2                    // pilih interface "pilih port, bridge" jika banyak bikin vlan tumpuk di ether/bridge ini saja
-    5. apply > Ok
-        - kalau mau kasih comment silahkan   // optional
-    6. buatkan juga untuk VLAN-20 nya di ether atau bridge yang sama dg interface VLAN-10
+    3. interface : ether2                    // pilih interface "port/bridge" jika banyak bikin vlan nantinya, tumpuk di ether/bridge ini saja
+    -  apply > Ok
+    -  buatkan juga untuk VLAN-20 nya di ether/bridge yang sama dg interface VLAN-10 td
+    -  loop protect : default(opt : on)      // supaya selalu on saja silahkan (di penjelasan lain kayaknya tidak usah)
 ----------
 IP ADDRESS VLAN
 kasih IP gateway pada kedua vlan 
@@ -38,27 +31,24 @@ kasih IP gateway pada kedua vlan
        misalnya : IP VLAN-10 : 10.10.10.10/22 dan IP VLAN-20 : 20.20.20.20/24
 ---------
 DHCP SERVER
-- setiap interface jika mau di bagikan kebawah maka buatkan IP server pada Interface bersangkutan (ETHER BRIDGE VLAN)
-  seperti IP DINAMIC DHCP SERVER, IP STATIS, HOTSPOT sesuai kebutuhan, dalam kasus ini dinamis
     1. IP > DHCP Server > DHCP > tombol DHCP Setup > interace : VLAN-10 > next2 // dalam kasus ini pilih VLAN10 (port, bridge, vlan)
         - terpenting perhatikan IP address ya (biasanya sih otomatis)
 ______________________________________________________________________________________
---------------------------------------------------------------------------------------
 TERIMA VLAN
 --------------------------------------------------------------------------------------
 pada vlan kita kenal dengan type port:
-port/intervace VLAN TRUNC  = port yang mebawa multi servevice
-port/intervace VLAN ACCESS = port membawa single service 
+port/intervace VLAN TRUNC  = port yg membawa multi service
+port/intervace VLAN ACCESS = port yg membawa single service 
 
-- pada OS versi 6 kebawah tidak suport hardware-offload oleh karena itu gunakan 6> keatas
-- sebelum melakukan pembuatan vlan kita harus tahu dulu apakah RB kita terdapat CHIP switch
-    - switch > type dan lihat vlan table kemampuanya di www.wiki.mikrotik.com > tiap RB chip nya berbeda2
-    - port > semua port tergabung dalam "switch1" > sehingga bisa di lewatkan access oleh chip ke swith tersebut
-      "switch1" di gunakan untuk setting vlan access nantinya
-    - kalau v6 kebawah ada "master port", v6 keatas di gantikan dengan "hardware ofload"
+- pada OS versi 6 kebawah tidak suport hardware-offload oleh karena itu gunakan v6 keatas
+- sebelum melakukan pembuatan vlan kita harus tahu dulu apakah RB kita terdapat CHIP switch nya
+    - switch > type dan lihat vlan list. kemampuanya di www.wiki.mikrotik.com > tiap RB chip nya berbeda2
+    - port > semua port tergabung dalam "switch1" > sehingga bisa di lewatkan access oleh chip ke "switch1" tersebut
+      "switch1" di gunakan untuk setting vlan access dan trunc nantinya
+    - kalau v6 kebawah ada "master port", v6 keatas di gantikan dengan "hardware offload"
 -------------------------
 - BIKIN BRIDGE
-    1. bridge > add > name.
+    1. bridge > add > name: bridge1-VLAN (misal)
     2. tab port > add > pilih port dari trunc ether-1 untuk masuk 2 dan 3 untuk keluar dan bridge yang di buat > 
        centang "hardware ofload" (sebagai ganti dari master port di v6 kebawah)
 - BIKIN VLAN ACCESS(memecah vlan keluar)
