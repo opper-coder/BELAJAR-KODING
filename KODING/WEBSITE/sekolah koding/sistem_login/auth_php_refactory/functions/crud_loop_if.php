75b227378e43c5df0eb6e0c5a6abe19d21ff9ea6
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-17 06:43:43 +0800

    Update crud_loop_if.php

--- KODING/WEBSITE/sekolah koding/sistem_login/auth_php_refactory/functions/crud_loop_if.php
+++ KODING/WEBSITE/sekolah koding/sistem_login/auth_php_refactory/functions/crud_loop_if.php
@@ -1,1 +1,22 @@
-
+BUATKAN TEMPLAT FUNCTION UNTUK 
+------------------
+TAMPILKAN
+1. tampilkan data tunggal
+2. tampilkan data list
+3. Tampilkan data pagination
+4. tampilkan data sorting satu parameter (if)
+5. tampilkan data sorting multi parameter (if)
+------------------
+TULIS DATA
+1. 
+2. 
+------------------
+UPDATE
+1. 
+2. 
+3. 
+------------------
+DELETE DATA
+1. hapus permanent
+2. hapus tandai
+3. tabel hapus tandai 
