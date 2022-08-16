bbbec4739a9c6475c9e9f68d652633a2018d3f56
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-16 21:52:06 +0800

    Update user.php

--- KODING/WEBSITE/sekolah koding/sistem_login/auth_php_refactory/functions/user.php
+++ KODING/WEBSITE/sekolah koding/sistem_login/auth_php_refactory/functions/user.php
@@ -1,57 +1,50 @@
 <?php
 // -----
-function register_user( $nama, $pass ){
-    global $conn;
-    $nama = mysqli_real_escape_string($conn, $nama);
+function register_user( $nama, $pass ){                                           // INPUT NAMA, BERSIHKAN KARAKTER, ENCRYPT, QUERY INSERT DB
+    global $conn;                                                                       // $conn global dibuat sekali, agar bisa query
+    $nama = mysqli_real_escape_string($conn, $nama);                                    // bersihkan karakter code
     $pass = mysqli_real_escape_string($conn, $pass);
-    $pass = password_hash( $pass, PASSWORD_DEFAULT);
-
-    $queryinsert = "INSERT INTO users (username, password) VALUES ('$nama', '$pass')";
+    $pass = password_hash( $pass, PASSWORD_DEFAULT);                                    // encryption password input
+    $queryinsert = "INSERT INTO users (username, password) VALUES ('$nama', '$pass')";  // syntax query input data register ke db
     // variabel = " QUERY database ( field1, field2 )" VALUES ( 'input1','input2' )";
-
-    if( mysqli_query ($conn, $queryinsert) ){
+    if( mysqli_query ($conn, $queryinsert) ){                                           // cek query berhasil/tidak(eksekusi query disini)
         // echo 'berhasil';
-        return true;
+        return true;                                                                    // bool
         }else{
         // echo 'gagal';
-        return false;}
+        return false;}                                                                  // bool
     }
 // -----
-function register_cek_nama($nama){
-    global $conn;
-    $nama = mysqli_real_escape_string($conn, $nama);
-
-    $query = "SELECT * FROM users WHERE username = '$nama'";
-    if( $result = mysqli_query( $conn, $query ) ){
-        if(mysqli_num_rows($result) == 0) return true;
+function register_cek_nama($nama){                                                 // CEK NAMA SUDAH ADA? bool
+    global $conn;                                                                       // ambil g conn
+    $nama = mysqli_real_escape_string($conn, $nama);                                    // sanitize
+    $query = "SELECT * FROM users WHERE username = '$nama'";                            // cari di db
+    if( $result = mysqli_query( $conn, $query ) ){                                      // execute dan chect db
+        if(mysqli_num_rows($result) == 0) return true;                                  // action
         else return false;
     }
 }
 // -----
-function cek_pass($nama, $pass){
-    global $conn;
-        $nama = mysqli_real_escape_string($conn, $nama);
-        $pass = mysqli_real_escape_string($conn, $pass);
-        
-        $query = "SELECT password FROM users WHERE username = '$nama'";
-        $result = mysqli_query($conn, $query);
-        $hash = mysqli_fetch_assoc($result)['password'];
-
-    if (password_verify($pass, $hash)) {
+function cek_pass($nama, $pass){                                                    // CEK PASSWORD ada? bool
+    global $conn;                                                                       // ambil g conn
+        $nama = mysqli_real_escape_string($conn, $nama);                                // sanitize
+        $pass = mysqli_real_escape_string($conn, $pass); 
+        $query = "SELECT password FROM users WHERE username = '$nama'";                 // cari password dari nama di db
+        $result = mysqli_query($conn, $query);                                          // eksekusi
+        $hash = mysqli_fetch_assoc($result)['password'];                                // ambil pass
+    if (password_verify($pass, $hash)) {                                                // urai encrypt. dan cek berhasil?
         // die('berhasil');
-        return true;
+        return true;                                                                    // aksi
     } else {
         // die('gagal');
         return false;
     }
 }
 // -----
-function login_cek_nama($nama){
+function login_cek_nama($nama){                                                     // CEK NAMA ada? bool
     global $conn;
     $nama = mysqli_real_escape_string($conn, $nama);
-
-    $query = "SELECT * FROM users WHERE username = '$nama'";
-
+    $query = "SELECT * FROM users WHERE username = '$nama'";                            // cari nama sesuai input nama
     if( $result = mysqli_query( $conn, $query ) ){
         if(mysqli_num_rows($result) != 0) return true;
         else return false;
