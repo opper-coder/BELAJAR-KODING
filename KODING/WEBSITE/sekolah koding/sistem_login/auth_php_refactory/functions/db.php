d1b810c355a19c70bd09dfce70988cb32e217eaf
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-17 18:21:07 +0800

    Update db.php

--- KODING/WEBSITE/sekolah koding/sistem_login/auth_php_refactory/functions/db.php
+++ KODING/WEBSITE/sekolah koding/sistem_login/auth_php_refactory/functions/db.php
@@ -1,15 +1,11 @@
 <?php
-
 $host       = "localhost";
 $user       = "root";
 $password   = "";
 $db         = "sekolahkoding";
 
 $conn = mysqli_connect ($host, $user, $password, $db) or DIE ( mysqli_error() );
-
 ?>
-
-
 <!--  
 langsung kita hentikan jika salah konek dengan die
 jika tak error lanjut di init.php jika error munculin mati dan munculin errornya
