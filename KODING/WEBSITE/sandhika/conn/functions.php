4ea045dca6ca8e15859e6f8034b2b5a3dc9549c5
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-17 11:48:42 +0800

    Update functions.php

--- KODING/WEBSITE/sandhika/conn/functions.php
+++ KODING/WEBSITE/sandhika/conn/functions.php
@@ -1,13 +1,12 @@
 <?php
-// koneksi biasa
-$conn = mysqli_connect("localhost","root","","belajardb");  	// tinggal cut paste 'koneksi' dari halaman 1 di index																			// buat fungsi query dan fetching agar tingal panggil di halaman index
-function query($query){						// bikin fungsi query
-    global $conn;						// pakai global $conn
-    $result = mysqli_query($conn, $query);			// lakukan queri biasa dg para meter ambi dari  argumen func
-    $rows = [];							// siapkan var rows arai kosong di  luar while uuntk di isi dalam while
-    while( $row = mysqli_fetch_assoc($result)){			// while kan array (yg di dapat dari vetching row)
-        $rows[] = $row;						// isi array rows yg di siapkan dengan pengulangan (row berisi array vetching)
+$conn = mysqli_connect("localhost","root","","belajardb");  // buat fungsi query dan fetching agar tingal panggil di halaman index
+function query($query){						                // bikin fungsi query
+    global $conn;						                    // pakai global $conn
+    $result = mysqli_query($conn, $query);			        // lakukan queri biasa dg para meter ambi dari  argumen func
+    $rows = [];							                    // siapkan var rows arai kosong di  luar while uuntk di isi dalam while
+    while( $row = mysqli_fetch_assoc($result)){			    // while kan array (yg di dapat dari vetching row)
+        $rows[] = $row;						                // isi array rows yg di siapkan dengan pengulangan (row berisi array vetching)
     }
-    return $rows;						// kembalikan rows
+    return $rows;						                    // kembalikan rows
 }
 ?>
