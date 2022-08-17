ef852cadd4c0a42606d838bde79fefd72ca1b947
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-08-17 08:56:20 +0800

    Update koneksi standard php.php

--- KODING/WEBSITE/sandhika/standar db konn/koneksi standard php.php
+++ KODING/WEBSITE/sandhika/standar db konn/koneksi standard php.php
@@ -1,33 +1,16 @@
-
 <?php 
-
-$conn = mysqli_connect("localhost","root","","belajardb");
-$result = mysqli_query($conn,"SELECT * FROM mahasiswa");
-$row = mysqli_fetch_row($result);
-var_dump($row[2]);
-echo($row[1]);    // sudah bisa di ambil datanya
-
-
-
-
+$conn = mysqli_connect("localhost","root","","belajardb");	// koneksi ke DB
+$result = mysqli_query($conn,"SELECT * FROM mahasiswa");	// eksekusi(syntax query)
+$row = mysqli_fetch_row($result);				// ambil data record (bisa di looping semua record)
+var_dump($row[2]);						// ambil data pada field row 
+echo($row[1]);    						// sudah bisa di ambil datanya
  ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>saiti mobile</title>
 </head>
 <body>
-
-
-
-
-<h1>hello world</h1>
-
-
-
-
+	<h1>hello world</h1>
 </body>
 </html>
