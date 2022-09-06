5de5051136cc7868183ca01f564f285e3f46d0ed
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-06 13:38:08 +0800

    Update i8-async-await.js

--- KODING/JS/i8-async-await.js
+++ KODING/JS/i8-async-await.js
@@ -73,4 +73,24 @@ hasilnya juga tidak sama saat console
 tapi itu kita praktekan dari promise, yang di dalamnya ada resolve() dan reject()
 nah bagaimana kalau kita pakai fetch() ini juga promise tapi yang kita tahu merthod nya 
 resolve saja, sedang rejectnya kita belum tahu langsung saj kita praktek pada kasus
-yang nyata saja yaitu menggunakan rest API online beneran */
+yang nyata saja yaitu menggunakan rest API online beneran 
+
+ada 3 error yang kita akan coba atasi
+1. seach film yang tidak ada
+2. search kosong
+3. url kita kasih error
+
+-	yang kita tahu kalau kita menggunakan fetch(url).then() dan datanya "tersedia" 
+	maka itu kita sebut "resolve" 
+- nah yang jadi masalah adalah ketika tearjadi 3 error di atas 
+	bagaimana cara mengetahui "reject" nya fetch seperti pada promise
+- yang kita lakukan adalah kita cari tahu Response pada api nya,
+	biasanya terdapat parameter pada REST API nya atau 
+	coba akses console.log(response.Response) pada hasil fetcing nya
+	response yang "pertama" adalah response dari pemberian nama kita 
+	pada hasil fetch dalam then(). sedang "Response" R besar yang kedua 
+	adalah Response "Key dari API OMDB" pada contoh di video
+- lalu kita
+
+*/
+
