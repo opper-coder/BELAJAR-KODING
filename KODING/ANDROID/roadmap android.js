1415a7ea6f17371280be74e8c98b5020ef6652fd
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-07 23:03:52 +0000

    Create roadmap android.js

---
+++ KODING/ANDROID/roadmap android.js
@@ -0,0 +1,228 @@
+// =================================
+1 komponen dan attribute
+2 style
+3 tiga layout
+4 activity dan fragment
+4 navigasi
+5 oper data
+6 animasi
+7 lifecycle
+8 recylerview
+
+9 viewmodel database
+10 db sqlite dan room
+11 caching
+12 db API
+13 db firebase
+14 API Android
+15 map api
+
+16 integrasi
+// =================================
+// ---
+1 Belajar android untuk pemula
+
+- ❱ Intro dan Setup
+- Intro android
+- Install dan project android pertama
+- Menjalankan virtual device android
+- Menjalankan di device langsung
+- Overview project android
+
+❱ Dasar Element dan Action
+- Menambahkan button dan action
+- Mengenal linear layout
+- Extract nilai string
+- Mengambil text berdasarkan input
+Memasukkan gambar di android
+Mengubah gambar jadi dinamis
+Mengganti syntetic kotlin dengan data binding 
+
+// ---
+2 Belajar design dan layout android
+Intro design android
+
+❱ Jenis-jenis layout
+Mengenal linear layout
+Mengenal relative layout
+Mengenal constraint layout
+Constraint layout part
+Latihan constraint layout
+
+❱ Attribute
+Mengatur padding, border dan margin
+Style font dan dimens
+Extract style beberapa attribute
+Thema style general vs attribute
+Latihan constraint layout twitter 
+
+// ---
+3 Navigasi di Android
+
+❱ Perkenalan Navigasi
+Trailer navigasi android
+Single activity vs multiple activites
+
+❱ Mengenal Fragment
+Membuat fragment pertama
+Design intro fragment
+
+❱ Membuat game sederhana
+Membuat fragment quiz
+Navigation component dan migrasi androidx
+Klik button untuk ganti fragment
+Data binding pada fragment quiz
+Logika fragment quiz
+
+❱ Data pada beberapa halaman
+Finish fragment dan install safe args
+Passing data antara fragment
+
+❱ Actionbar dan back button
+Mengatur backstack fragment
+Menambah up button di actionbar
+Mengganti title fragment di actionbar
+
+❱ Menu di android
+Overflow menu di actionbar
+Menu drawer slider
+Membuat bottom navigation
+
+❱ Animasi
+Membuat animasi
+
+❱ Implicit dan explicit intent
+Explicit intent ke activity lain
+Mengoper data antar activity
+Contoh implicit intent 
+
+// ---
+4 Lifecycle pada aplikasi Android
+Intro lifecycle
+OnCreate dan onStart
+OnResume dan onPause
+OnStop, onRestart dan onDestroy
+Efek rotasi dan save instance
+Simulasi lifecycle dengan timer
+Info seputar lifecycle aware component 
+
+// ---
+5 Viewmodel dan LiveData di Android
+
+❱ Intro
+Intro architecture component viewmodel code avaiable logo
+Project scoring app tour code avaiable logo
+
+❱ ViewModel
+Mulai menggunakan viewModel
+Mengatur package agar lebih rapi
+Memindahkan data ke viewModel
+Refactor kode dan update UI
+
+❱ LiveData
+Menggunakan liveData
+Encapsulation memproteksi data
+Event game finished
+Refactor data ke viewModel
+
+❱ DataBinding
+Databinding dan viewModel
+Databinding dan liveData
+Transformation map mengolah data 
+
+// ---
+6 RecyclerView pada Android
+
+❱ Intro
+Intro recylerview
+Tampilan dasar aplikasi recyclerview
+Cara menggunakan recyclerview
+
+❱ CRUD aplikasi
+Menambahkan data baru
+Menghapus data
+Mengupdate data
+Data binding di adapter code avaiable logo
+
+❱ viewModel
+Menyiapkan viewModel
+
+❱ DiffUtil
+Menggunakan diffUtil dan listAdapter
+Observe submitList dan metode viewModel
+Membuat id dinamis 
+
+// ---
+7 Daftar isi Belajar database menggunakan SQLite
+
+Perkenalan dasar database
+Apa sih database sebenarnya? dan apa itu konsep table, kolom dan tipe data di dalamnya. Kita akan berkenalan dengan dasar-dasar database di sini
+Membuat database dan table baru
+Waktunya untuk mulai membuat database dan table baru pada sqlite
+Menambah data
+Bagiamana cara menambah data baru dengan query di sqlite
+Update dan hapus data
+Belajar bagaimana cara mengupdate dan menghapus data dengan query di sqlite
+Select where dan like
+Kita juga bisa memilih kolom tertentu menggunakan query where dan like untuk menfilternya pada sqlite
+Limit dan order
+Mau membatasi jumlah data yang ditampilkan? gunakan limit. Mau mengurutkan data berdasarkan kolom tertentu pada database? bisa menggunakan ORDER BY
+
+// ---
+8 Belajar database dengan Room di Android
+
+❱ Persiapan Room
+Intro room
+Deklarasikan room
+Membuat Entity dan DAO
+Database di room
+
+❱ Implementasi Room
+Repository di viewModel
+Masalah main thread
+Menggunakan coroutine
+Menghapus data room
+
+❱ Shared Preferences
+Shared preferences intro
+Cara save dan load shared preference 
+
+// ---
+9 Mengkonsumsi API di aplikasi android
+ 
+Intro api android
+Melihat github api
+Tour basic project code avaiable logo
+Permission internet dan install retrofit
+Menggunakan retrofit untuk request
+Converter moshi untuk json
+Coroutine pengganti enqueue
+Recyclerview data api
+Menampilkan gambar dengan glide
+Onclick pada gambar
+Menerima parameter safeargs di detailview
+Detail view selesai
+Status loading dan error
+Tantangan melanjutkan 
+
+// ---
+10 Caching data API/network di android
+
+❱ Persiapan API
+Intro caching data api code avaiable logo
+Project tour caching code avaiable logo
+Membuat api dengan json server
+Menyiapkan database room
+
+❱ Sistem Cache
+Membuat repository
+Menggunakan cache database
+Mengenal coroutine worker
+
+❱ Background Task
+Worker untuk background task
+Menjadwalkan worker
+Memberi constraint untuk worker code avaiable logo 
+
+// ---
+11 
