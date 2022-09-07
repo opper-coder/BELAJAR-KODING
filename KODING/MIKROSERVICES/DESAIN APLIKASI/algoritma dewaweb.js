e8ef4b9245c6830afcb33746276770fbee61f87b
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-07 23:09:31 +0000

    Create algoritma dewaweb.js

---
+++ KODING/MIKROSERVICES/DESAIN APLIKASI/algoritma dewaweb.js
@@ -0,0 +1,24 @@
+BRD sketch
+
+harus ada solusi offline dari semua peristiwa di online
+	misalnya penyelesaian pembayaran saat mati connection
+	
+tips dewa web
+1. pahami masalah yang di hadapi pasar saat ini
+2. menilai apa yang dilihat konsumen dari kompetitor
+	- cek web site nya
+	- bagaimana menempatkan diri pada konsumen
+	- image yang di tampilkan
+	- pesan apa dalam iklan nya
+	- kelebihan 
+		- selling point
+		- fitur
+		- value
+		- layanan
+		- kwalitas bahan
+3. analisis preferensi atau ketertarikan konsumen
+	- meminta tetimoni atau wawancara
+4. riset pasar
+	- demografi usia, jenis kel, temp tinggal, ekonomi, status
+5. kembangkan ide
+6. digitalisasi bisnis
