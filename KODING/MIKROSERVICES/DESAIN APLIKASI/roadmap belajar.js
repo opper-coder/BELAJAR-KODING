a1b4975d15c62707ee1b19c8910f456fda65699c
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-07 23:02:45 +0000

    Create roadmap belajar.js

---
+++ KODING/MIKROSERVICES/DESAIN APLIKASI/roadmap belajar.js
@@ -0,0 +1,46 @@
+ ALUR BELAJAR DAN MANAJEMEN FOLDER BELAJAR
+
+restful api kotlin
+
+CATATAN POTRET SCREENSHOOT
+
+KONSEP
+	micro services 19 x 15 = 238			#perkenalan
+	desain patern 13 x 10 =130 				#sebagian
+	softaware arsitektur 9 x 15 = 140 		##sudah
+
+BAHASA
+	go lang pemula 58 x 10 = 580
+	go lang 2: 35, 1.47 = 2.17 jam
+	kotlin kolection = 42 x 10 = 450
+	kotlin generic = 16 x 10 = 160
+	kotlin oop = 51 x 10 = 500
+	kotlin dasar = 42 x 10 = 420
+
+DATABASE
+	my sql	-> cache (premium) 1: --		
+	mongodb 31 x 11 = 330
+	redis 20 x 10 = 180
+	restfull API 21 x 12 = 250 				##sudah
+
+ENVIRONTMEN
+	kotlin unit test 42 x 10 = 420 menit
+	gradle 11 x 9 = 100 menit
+	kubernetis 50 x 10 = 500
+	apache kafka 17 x 10 
+	elastic search pemula 18 x 10 = 180	
+	docker 23 x 10 = 230 
+
+5120 menit	85,33333333333333	jam 10,666666666666666 hari
+
+DISARANKAN
+
+belajar
+
+- microservice
+- relational database dbms
+- restfull API laravel
+- atau mongodb
+- oauth2
+
+-
