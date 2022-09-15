db49c85edcf35482fc2bd91424cb29242ef7b156
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-15 08:59:24 +0800

    Update i1-konsep-overview.js

--- KODING/firebase/i1-konsep dan persiapan/i1-konsep-overview.js
+++ KODING/firebase/i1-konsep dan persiapan/i1-konsep-overview.js
@@ -1,6 +1,12 @@
 /* KONSEP PROJECT
 -----------------------------------------------------------------------------------
-
+CARA MENGGUNAKAN DOKUMENTASI INI
+- dalam dokumentasi ini di buat per service firebase satu halaman
+- cara baca per halaman pun hanya menunjukan cara pandang di dokumentasi aslinya
+- di halaman ini hanya memberikan pengantar untuk tetap merefensikan pada dokumentasi aslinya
+- di dokumentasi ini pengantar saja karena saya hanya menagmbil alur argumentasinya
+  sehingga secara praktek sebenarnya bisa di lakukan sendiri dengan copas di sumbernya  
+- banyak pada halaman ini belum tuntas karena menyelesaikan gagasan idenya
  */
 
 /* HIERARKI PROJECT
