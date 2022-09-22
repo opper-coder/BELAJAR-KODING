b1a751574550598bc6562e45bebc0d5bb64cdabb
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-23 06:44:39 +0800

    Update i4-queuetree-HTB.js

--- KODING/MIKROTIK/QoS/i4-queuetree-HTB.js
+++ KODING/MIKROTIK/QoS/i4-queuetree-HTB.js
@@ -2,8 +2,7 @@
 ---------------------------------------------------------------------------------------------------
 - konsep 		-> konfig dasar (kombinasi paket mark dan HTB)
 - packet mark 		-> disinilah paket di buat
-- HTB simple queue 	-> HTB pada simple queue juga bisa
-- HTB queue tree 	-> disini 
+- HTB queue tree 	-> implementasi
 - HTB 			-> hirarki prioritas (HTB inilah yang jadi perhatian dalam queue tree)
 
 QUEUE TREE DAN HTB
@@ -58,27 +57,27 @@ HTB adlah implementasi hirarki orang tua, anak, cucu dst untuk mengatur priority
 4. bikin queue tree untuk upload (jadi child nantinya)
 	queue > tab queue tree > add 
 		tab general >
-			name: queue1-upload 			// nama(terserah) 
-			parent: bridge-WAN 			// jalur internet (di ubah ke parent, nantinya)
-			packet mark: packet-cliet1 		// (pilih yg sudah di buat) 
-			queue type: pilih 			// ada pembahasan sendiri
-			max limit: 10M    			// max limit untuk upload
+			name: queue1-upload 		// nama(terserah) 
+			parent: bridge-WAN 		// jalur internet (di ubah ke parent, nantinya)
+			packet mark: packet-cliet1 	// (pilih yg sudah di buat) 
+			queue type: pilih 		// ada pembahasan sendiri
+			max limit: 10M    		// max limit untuk upload
 		apply > OK : akan terlihat ada traffic pada field avg. rate 
 
 5. implementasi HTB. yaitu bikin dulu queue yang jadi parent 
 	queue > tab queue tree > add 
 		tab general 
-			name: queue1-upload-parent 		// nama parent(terserah) 
-			parent: bridge-WAN 			// jalur internet
-			packet mark: 				// kosongkan (yg punya packet adalah childnya)
-			queue type: pilih 			// sesuaikan saja
-			max limit: 20M    			// max limit untuk upload parent
+			name: queue1-upload-parent 	// nama parent(terserah) 
+			parent: bridge-WAN 		// jalur internet
+			packet mark: 			// kosongkan (yg punya packet adalah childnya)
+			queue type: pilih 		// sesuaikan saja
+			max limit: 20M    		// max limit untuk upload parent
 		apply > OK : akan terlihat ada traffic pada field avg. rate 
 
 6. masukan hirarki parent child
 	pada perent di child ubah jadi queue-uopload-parent
-		parent: queue1-upload-parent			// ubahan dari bridge-WAN (ganti parent)
-		apply > OK 					// akan terlihat hirarkinya
+		parent: queue1-upload-parent		// ubahan dari bridge-WAN (ganti parent)
+		apply > OK 				// akan terlihat hirarkinya
 
 */
 
@@ -87,17 +86,17 @@ HTB adlah implementasi hirarki orang tua, anak, cucu dst untuk mengatur priority
 HTB bisa juga pada simple queue langkah2 nya
 - bikin child child nya dulu
 	queue > tab simple queue > tab general
-		name: queue1-child 		// nama terserah
-		target: 10.10.10.1 		// client
+		name: queue1-child 			// nama terserah
+		target: 10.10.10.1 			// client
 		max limit : Ul 5M Dl 5M  
 - bikin parent:
 	queue > tab simple queue > tab general
-		name: queue1-parent 		// nama terserah
-		target: 10.10.10.0/24 		// client harus di masukkan semua di target pada parent (berbeda dg quetree soal child)
+		name: queue1-parent 			// nama terserah
+		target: 10.10.10.0/24 			// client harus di masukkan semua di target pada parent (berbeda dg quetree soal child)
 		max limit : Ul 10M Dl 10M  
 - bikin HTB child parent. pilih queue child lalu hubungkan ke parent 
 	queue > tab simple queue > tab advanced
-		parent: queue1-parent 		// pilih parent yang sudah di buat
+		parent: queue1-parent 			// pilih parent yang sudah di buat
 	lalu sorting di field sesuai urutan parent
 */
 
@@ -109,8 +108,8 @@ berdasarkan:
 untuk dapat di alokasikan pola pembagianya 
 berdasarkan:
 	stagged limitation(limitasi bertahap):
-	CIR: committed information rate 	// limit at: yaitu semua di kasih limit at kalau memang punya bandwidth
-	MIR: maximal information rate 		// max limit: jika ada sisa maka yg masuk prioritas akan di beri bonus: max limit
+	CIR: committed information rate 		// limit at: yaitu semua di kasih limit at kalau memang punya bandwidth
+	MIR: maximal information rate 			// max limit: jika ada sisa maka yg masuk prioritas akan di beri bonus: max limit
 
 susunan HTB
 leave queue: adalah queue yang langsung di pakai client
