224d53d8b821d69a26e870c32b77b900a3094fc0
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-30 20:26:29 +0800

    Update i1-persiapan.js

--- KODING/MIKROTIK/Aplikasi/android/i1-persiapan.js
+++ KODING/MIKROTIK/Aplikasi/android/i1-persiapan.js
@@ -1,1 +1,17 @@
-
+/* APlIKASI ANDROID MIKRO-ITS
+---------------------------------------------------------------------------------------------------
+Aplikasi android
+	- topUP > kirim resi pembayaran > 
+	- user account
+	- saldo
+	- chat
+		- grup
+		- inbox
+	- Generate voucher
+	- voucher pengganti listrik mati
+	- daftar accounting voucher
+	- cairkan
+	- jam tanggal
+	- monitoring bandwidth bagus tidaknya
+  - gratis bandwidth hanya untuk aplikasi MIKRO-ITS, WA, facebook, telegram dll 
+*/
