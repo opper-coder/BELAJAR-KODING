e5927d65784005e477278e44a65bb26c22961e7a
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-30 19:46:19 +0800

    Update i1-persiapan.js

--- KODING/MIKROTIK/Aplikasi/dashboard/i1-persiapan.js
+++ KODING/MIKROTIK/Aplikasi/dashboard/i1-persiapan.js
@@ -1,1 +1,18 @@
-
+/* APlIKASI DASHBOARD MIKRO-ITS
+---------------------------------------------------------------------------------------------------
+DASHBOARD
+	- admin keuangan dengan hirarki network bussiness
+	- monitoring
+		- monitoring beberapa server
+		- monitoring aktifitas perangkat
+	- notikasi 
+	- chat admin
+	- sistem user
+	- client
+		- Hotspot
+		- PPPoE
+	- setting
+		- voucher profile
+		- 
+	- topUP
+*/
