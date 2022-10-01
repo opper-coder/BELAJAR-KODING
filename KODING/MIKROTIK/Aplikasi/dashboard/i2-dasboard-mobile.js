57cab9d36353325496d0045a58c48f839f757549
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-10-01 18:36:24 +0800

    Create i2-dasboard-mobile.js

---
+++ KODING/MIKROTIK/Aplikasi/dashboard/i2-dasboard-mobile.js
@@ -0,0 +1,16 @@
+/* 
+--------------------------------------------------------------------------------------------------
+monitoring
+  jaringan
+  mati listrik
+chat
+  kontak
+  inbox
+  group
+voucher
+  create
+  blokir
+  ganti
+  lanjutkan voucher mati PLN
+TOP UP
+*/ 
