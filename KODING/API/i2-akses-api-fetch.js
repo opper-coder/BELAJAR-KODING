26ecd85d7f01f53a8b2519012374c611b7d6e874
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-10-02 11:37:25 +0800

    Update i2-akses-api-fetch.js

--- KODING/API/i2-akses-api-fetch.js
+++ KODING/API/i2-akses-api-fetch.js
@@ -6,6 +6,7 @@ API
 	- ajax jquery, basic		-> bisa GET saja (sebenarnya bisa GET POST, UPDATE, PUT, DELETE PATCH)
 	- ajax fetch() basic		-> Hanya GET
 	- fetch() CRUD 			-> lebih advance melakukan CRUD ke API (UPDATE DELETE NYA BELUM ADA YA)
+	- axios() 			-> kalau mau library third party kita bisa download dengan npm sepertinya lebih ringkas dalam penulisanya
 */
 
 
@@ -220,6 +221,11 @@ DELETE
 belum ada
 */
 
+/*
+AXIOS
+---------------------------------------------------------------------------------------------------
+belum ada ada di github
+*/
 
 
 
