fd65853546fd5e3dd3e87201c10563ecdabe5cda
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-10-08 13:52:25 +0800

    Update i1-persiapan.js

--- KODING/MIKROTIK/Aplikasi/dashboard/i1-persiapan.js
+++ KODING/MIKROTIK/Aplikasi/dashboard/i1-persiapan.js
@@ -4,6 +4,7 @@ DASHBOARD
 	- admin keuangan dengan hirarki network bussiness
 	- monitoring
 		- monitoring beberapa server
+			- name space
 		- monitoring aktifitas perangkat
 		- mikrotik status
 		- mati listrik
@@ -38,4 +39,33 @@ DASHBOARD
 		- FUP
 		- TOPUP
 	- setting mikrotik
+	- inventaris
+		- toolkit
+			- torch 
+			- OPM 
+			- OTDR skycom
+			- Splitcher skycom
+			- splacer harga 1,5 juta
+			- tang potong FO
+			- tang crimping RJ45
+			- obeng 
+			- alat 
+			- kabel vlan poe
+		- perangkat network
+			- MIKROTIK RB450gx4	/ 100 - 200 user
+			- OLT HIOSO HA7302CS	/ 2 PON 64x2 = 128 
+			- ONU / ONT
+			- kabel rj45
+				- MIKROTIK ke OLT
+				- ISP ke Mikrotik
+				- remote ke laptop
+			- Splitter
+			- HTB
+		- sarana 
+			- motor
+			- tangga
+			- HT
+			- pulsa HP
+			- senter
+			
 */
