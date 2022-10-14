fab81e047858f114c0a53e2d6f0aa8a427491d3a
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-10-14 21:46:18 +0800

    Update i3-mode router.js

--- KODING/MIKROTIK/Router/i3-mode router.js
+++ KODING/MIKROTIK/Router/i3-mode router.js
@@ -4,38 +4,37 @@ secara garis besar kemampuan router adalah:
 1. menangkap intenet
 2. mendistribusikan internet yang di miliki
 secara lengkap memiliki kemampuan:
-	WAN 				: menangkap wifi (internet) dari server 
-	WWAN 				: 
-	LAN 				: mendistribusikan wifi ke pengguna melui ETHER
-	WLAN 				: mendistribusikan wifi ke pengguna melui wifi lagi
-	DHCP Server 		:
-	baca MAC 			:
-	remote 				: biasanya satu sgmen dan harus eneble kan mode remote
+	WAN 				: menangkap wifi (internet) dari server melalui kabel di port WAN(tdk semua router ada)
+	WWAN 				: menangkap wifi (internet) dari server melalui wireless 
+	LAN 				: mendistribusikan wifi ke (internet) pengguna melalui port LAN
+	WLAN 				: mendistribusikan wifi ke (internet) pengguna melalui wireless
+	DHCP Server 		: kemapuan mendistribusikan dengan generate IP, sehingga bisa membaca MAC clientnya di router
+	remote 				: biasanya satu segmen dan harus eneble kan mode remote
+	baca MAC di winbox	: 
 -----------------------------------------------------------------------------------------------------------------------
 MODE:
 - mode router
-	adalah mode default pabrik: adalah mode yang memasukan internet via ether WAN dan keluar via LAN dg IP yng berbeda
-	- dari mikrotik dikirim iinternet dengan 192.168.88.1
+	adalah mode default pabrik: masuk keluar via PORT
+    memasukan internet via PORT-WAN ke PORT-LAN, dg DHCP server ON. shg keluar via LAN dg IP yang berbeda
+	- dari mikrotik dikirim internet dengan 192.168.88.1
 	- di router di terima 88.1 di port WAN > pada dhcp server terlihat ON > 
-	   sehingga di kirim ke laptop dengan IP 192.168.0.1 melalui ether LAN
-	- beda segmen
+	  sehingga di kirim ke laptop dengan IP 192.168.0.1 melalui ether LAN (sudah beda segmen)
 	- mode ini bisa membaca di router > saat di lihat di dhcp client router perangkat terlihat
-	- di winbox tidak bisa lihat erangkat> melainkan router saja
-	
-- ROUTER			: 			
-- ACCESS POINT		: 
-- WISP 				: 		
-- CLIENT			: tangkap dari wifi. distrib ke kabel LAN(wifi, dhcp: off) tidak semua router support client
-- UNIVERSAL REPEATER: exstend saja (SSID ikut sumber, remote g bisa)
+	- di winbox tidak bisa lihat perangkat> melainkan router saja
+- ROUTER			: keluar masuk ETHER to ETHER. dhcp server(beda segmen)(WAN masih support jg)
+- ACCESS POINT		: sama tapi DHCP: OFF
+- WISP 				: keluar masuk WIRELESS to WIRELESS. (mirip router) 
+- CLIENT			: tangkap dari Wireless. distrib ke kabel LAN(wifi, dhcp: off) tidak semua router support client
+- UNIVERSAL REPEATER: mode perpanjang saja (SSID ikut sumber, remote g bisa)
 - BRIDGE			: internet masuk di LAN(bukan WAN mirip switch yang memiliki wifi)di sebar lewat LAN(kehilangan WAN dan WWAN)remote bisa
 
 tabel perbandingan:
------------------------------------------------------------------------------------------------------------------------
-mode					WAN			WWAN		LAN 		WLAN 		DHCP server 		baca MAC (di router dan mikrotik)		remote 		SSID
-ROUTER 					true 		-			true 		true 		true				true 									true		
-ACCESS POINT 			true 		- 			true 		true 		-					-										-			
-WISP 					-			true		true 		true		true				true									true	
-CLIENT					-			true		true 		-			-					-(tp device trkonek ada di mikro)		true		
-UNIVERSAL REPEATER		-			true		true 		true 		- 					- 										-			-
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+mode					WAN			WWAN		LAN 		WLAN 		DHCP server 		baca MAC (di router dan mikrotik)		remote 		SSID		WINBOX dhcp server > leases
+ROUTER 					true 		-			true 		true 		true				true 									true		true		router 
+ACCESS POINT 			true 		- 			true 		true 		-					-										-						router, client
+WISP 					-			true		true 		true		true				true									true		true 		router
+CLIENT					-			true		true 		-			-					-(tp device trkonek ada di mikro)		true		true		client
+UNIVERSAL REPEATER		-			true		true 		true 		- 					- 										-			- 			router, client
 BRIDGE					-			- 			true 		true		-(bisa di ON kan)	-										true 		-
------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
