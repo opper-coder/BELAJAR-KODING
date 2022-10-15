98f71a6bafcb9475fe41cd2af8bc6ac55ed21b19
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-10-15 17:01:23 +0800

    Update i4-queuetree-HTB.js

--- KODING/MIKROTIK/QoS/i4-queuetree-HTB.js
+++ KODING/MIKROTIK/QoS/i4-queuetree-HTB.js
@@ -12,7 +12,13 @@ queue tree adalah methode manajemen bandwidth dengan pola hirarchi HTB. issyu um
 	- priority 		: jika di kombinasikan akan ada prioritas
 	- parent child		: basic hirarki
 	- HTB 			: pola susunan otoritas
-
+	simple queue ataupun qtree adalah implementasi HTB 
+	HTB = hirarkikal token bucket adalah limitasi hirarkikal tumpukan
+	- berguna 'hirarkikal' untuk divisi2 di kantor dsb jadi bukan hanya limitasi per user tapi juga per grup
+	- berguna juga mengatur 'hubungan' parent dan child
+	- juga mengatur 'priority' antar child
+	- juga mengatur child2 secara 'independen' misalnya queue type nya berbeda dsb
+---------------------------------------------------------------------------------------------------
 config sederhana parent, queue type, limitasi. tapi yang berpengaruh sebenarnya di paket marknya: 
 	queue > tab queue tree > add 
 		tab general >
