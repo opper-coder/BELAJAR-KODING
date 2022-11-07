0e83be16a30f76c69ce124a85654511dc18d8138
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-11-07 18:36:16 +0800

    Update i1-javascript dasar.js

--- KODING/JS/i1-javascript dasar.js
+++ KODING/JS/i1-javascript dasar.js
@@ -82,10 +82,18 @@ Pendahuluan
 	di js dlm tag src <script src=>
 - Program Hello World -------------------------
 	document.writeln("helloworld"); // di html 
-	console.log("hello world");		// di console
+	document.write();
+	hasilparentdom.innerHTML = "<p>Aku suka Javascript</p>"; 
+	window.alert("Hello World!");	// atau tanpa window boleh
+	console.log("hello world");	// di console
 	console.info("hello world");	// pengganti log
 	console.warn("hello world");	// berwarna kuning
 	console.error("hello world");	// merah
+
+
+Menggunakan Fungsi alert();
+
+Menggunakan innerHTML.
 - Komentar ------------------------------------
 	// /**/
 - Tipe Data Number ----------------------------
