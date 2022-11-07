5ec3f5ead366b58f43bda5174ad987c330ef0055
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-11-07 11:36:00 +0800

    Update i4-full-height.js

--- KODING/WEBSITE/css/i1-Layoting/i4-full-height.js
+++ KODING/WEBSITE/css/i1-Layoting/i4-full-height.js
@@ -1,5 +1,5 @@
 <style>
   html, body { height: 100%; margin: 0; }
-  .full-height { min-height: 100% background: yellow }
+  .full-height { min-height: 100%; background: yellow; }
 </style>
 <div class="full-height">Halo full bodi</div>
