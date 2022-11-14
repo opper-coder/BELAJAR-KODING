d8c5e8747dd8aba1c007ea92502fd608221ceb1f
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-11-14 05:16:11 +0000

    Create gatewayhotspot.js

---
+++ KODING/MIKROTIK/loginpage/gatewayhotspot.js
@@ -0,0 +1,26 @@
+/* DAFTAR ISI
+--------------------------------------------------------------------------------------------------------------------
+
+
+*/
+/* Fitur gateway hotspot
+--------------------------------------------------------------------------------------------------------------------
+gateway hotspot adalah otentikasi klien sebelum ke jaringan publik
+fiturnya adalah:
+- tersedia mode otentikasi database lokal, atau hosting RADIUS hosting jarak jauh
+- akun klien lokal atau hosting RADIUS
+- mode firewall mmberi izin ke laman WEB tertentu
+- modfikasi login page
+- ubah IP ke alamat yg valid
+- klient DHCP di beritahu tentang baerada di balik captive portal
+- efektif hanya di IP v4 oleh karena itu mengandalkan NAT firewall untuk nangani IP v4. V6 belum di dukung
+
+
+
+
+
+
+
+
+*/
+
