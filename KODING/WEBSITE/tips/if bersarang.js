9df626d6afc5f2657a2e115c49fc14370cc6fdf0
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-01-15 14:10:28 +0800

    Update if bersarang.js

--- KODING/WEBSITE/tips/if bersarang.js
+++ KODING/WEBSITE/tips/if bersarang.js
@@ -3,7 +3,7 @@ ternyata cara menyusun if bersarang seperti ini sangat efektif coba saja
 if(!true){                      // kondisi1 (semua pakai not agar mudah di jalankan)
   if(!true){                    // kondisi2
     if(!true){                  // kondisi3
-        echo "kondisi else";            // else (disini sebagai else smua kondisi)
-      }else{ echo "kondisi3";}       // aksi kondisi3
-    }else{ echo "kondisi2";}        // aksi kondis12
-  }else{echo "kondisi1";}          // aksi kondis11
+        echo "kondisi else";    // else (disini sebagai else smua kondisi)
+      }else{ echo "kondisi3";}  // aksi kondisi3
+    }else{ echo "kondisi2";}    // aksi kondis12
+  }else{echo "kondisi1";}       // aksi kondis11
