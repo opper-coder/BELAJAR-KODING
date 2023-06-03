77cf9c76a4ef7e79d8691a8eb4db56b478f854f2
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-03 15:30:41 +0800

    Update and rename i2-overview-produk.js to i2-konsep-dasar.js

--- KODING/firebase/FIREBASE 2023/i2-overview-produk.js
+++
@@ -1,9 +0,0 @@
-untuk mengenal lebih lanjut mengenai firestor sudah pernah di buat difolder diluar ini
-tapi kurang lebih memahami tentang
-1. perbandingan dan memilih Cloud Firestore atau Realtime Database
-2. model data
-3. type data
-4. library SDK client: nodejs, go, android, ios, flutter
-5. index
-6. lokasi
-7. paket data
