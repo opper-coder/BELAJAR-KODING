200918948600e84ee4f52efe6fc2b5993236c73d
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-04 13:12:59 +0800

    Update i6-hapus-data.js

--- KODING/firebase/FIREBASE 2023/i6-hapus-data.js
+++ KODING/firebase/FIREBASE 2023/i6-hapus-data.js
@@ -1,1 +1,26 @@
+MENGHAPUS KOLEKSI
+---------------------------------------
+Untuk menghapus seluruh koleksi atau subkoleksi di Cloud Firestore, 
+ambil semua dokumen dalam koleksi atau subkoleksi itu, lalu lakukan penghapusan. 
+Jika koleksi berukuran besar, sebaiknya Anda menghapus dokumen dalam beberapa batch kecil untuk menghindari error kehabisan memori. 
+Ulangi prosesnya sampai Anda berhasil menghapus seluruh koleksi atau subkoleksi.
 
+Untuk menghapus koleksi, diperlukan koordinasi permintaan penghapusan individual dengan jumlah tidak terbatas. 
+Jika Anda perlu menghapus seluruh koleksi, hanya lakukan penghapusan dari lingkungan server yang tepercaya. 
+Meskipun penghapusan koleksi dapat dilakukan dari klien seluler/web, tindakan itu berdampak negatif pada keamanan dan performa.
+
+Cuplikan di bawah ini telah disederhanakan dan tidak mencakup penanganan error, keamanan, penghapusan subkoleksi, atau pemaksimalan performa. 
+Untuk mempelajari lebih lanjut satu pendekatan yang disarankan untuk menghapus koleksi dalam produksi, lihat Menghapus Koleksi dan Subkoleksi.
+tapi dari sisi server misalnya nodejs. 
+// Deleting collections from a Web client is not recommended.
+
+MENGHAPUS DATA DENGAN FIREBASE CLI
+---------------------------------------
+Anda juga bisa menggunakan Firebase CLI untuk menghapus dokumen dan koleksi.
+terminal CLI :> firebase firestore:delete [options] <<path>>        // path mungkin kayak doc() periksa ?????
+
+MENGHAPUS DATA DENGAN KONSOL
+---------------------------------------
+Anda dapat menghapus dokumen dan koleksi dari halaman Cloud Firestore di konsol. 
+Menghapus dokumen dari konsol akan menghapus semua data bertingkat dalam dokumen tersebut, termasuk subkoleksi yang ada.
+informasi lebih lanjut tentang kode error dan cara menyelesaikan masalah latensi saat menghapus check out data, lihat halaman "pemecahan masalah".
