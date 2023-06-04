a15b0fdb16446ec56c145de28b7d530edad162ae
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-04 12:15:07 +0800

    Create CATATAN.js

---
+++ KODING/firebase/FIREBASE 2023/CATATAN.js
@@ -0,0 +1,18 @@
+SEPERTINYA MATERI YANG DIPERLUKAN BISA BIKIN APLIKASI OPPER
+-----------------------------------------------------------
+app check
+authentication
+firestore
+realtime database
+cloud messaging
+cloud function
+cloud storage
+hosting
+firebase ML
+
+extention
+Resizes images
+Cloud Firestore data with Algolia.
+payment with strip
+google pay
+geocode firestore
