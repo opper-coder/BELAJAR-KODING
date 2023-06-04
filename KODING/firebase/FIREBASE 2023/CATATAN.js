bba05b55818b27c881d970d480c73d8c67d65619
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-04 12:19:44 +0800

    Update CATATAN.js

--- KODING/firebase/FIREBASE 2023/CATATAN.js
+++ KODING/firebase/FIREBASE 2023/CATATAN.js
@@ -1,18 +1,19 @@
 SEPERTINYA MATERI YANG DIPERLUKAN BISA BIKIN APLIKASI OPPER
 -----------------------------------------------------------
-app check
-authentication
-firestore
-realtime database
-cloud messaging
-cloud function
-cloud storage
-hosting
-firebase ML
+app check         -> utility cek client firebase
+authentication    -> userman
+firestore         -> db baru
+realtime database -> db lama 
+cloud messaging   -> notif
+cloud function    -> backend
+cloud storage     -> disk
+hosting           -> web client
+firebase ML       -> AI
+google map        -> peta
 
-extention
-Resizes images
-Cloud Firestore data with Algolia.
-payment with strip
-google pay
-geocode firestore
+extention         
+Resizes images    -> resize 
+Cloud Firestore data with Algolia. -> search
+payment with strip -> e-wallet
+google pay        -> e-wallet
+geocode firestore -> kode untuk rute atau semacamnya
