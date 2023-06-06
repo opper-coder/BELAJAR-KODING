f01525469b930b4dc99b9be6614804c8a7306443
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-06 17:54:23 +0800

    Update CATATAN.js

--- KODING/MIKROTIK/speedtes/CATATAN.js
+++ KODING/MIKROTIK/speedtes/CATATAN.js
@@ -9,8 +9,6 @@ ada kemungkinan signal bagus tapi data nggak lewat.
 meski trougput 100mbps apakah benar data yang bisa di lewatkan 100mbps
 coba test 50mbps sampai maximal berapa (tergantung ISP) 
 
-
-
 1. download dulu bandwidth test.exe di mikrotik.com > jalankan di windows
 2. buka winbox > config > /tools > bandwidth test server >  enable:yes, autectication:yes (kalau dah g di pakai matikan bahaya)
 3. buka system > resource amati port yng di uji
@@ -20,6 +18,7 @@ coba test 50mbps sampai maximal berapa (tergantung ISP)
 6. saat local tx:1M | remote tx:1M di naikan ke 30M maka CPU naik Bandwidth juga naik
 			    
 APLIKASI WIFI 
+--------------------------------------------------
 1. moho di https://moho.ruijienetworks.com/static/homepager/en/index.htm
 sangant bagus testing wifi
 			    
