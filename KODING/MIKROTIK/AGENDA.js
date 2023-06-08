5f9890b10cb5f71774cdfa4576b1cc10b6f1d752
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-08 09:15:14 +0800

    Update AGENDA.js

--- KODING/MIKROTIK/AGENDA.js
+++ KODING/MIKROTIK/AGENDA.js
@@ -1,1 +1,12 @@
-MID teknologi : Cara Hemat Remote Perangkat Dengan Port Forwading Mikrotik
+- MID teknologi : Cara Hemat Remote Perangkat Dengan Port Forwading Mikrotik
+========================================
+mikrotik indonesia
+
+- Membuat Limitasi FUP dengan User-Manager
+- Mengatasi MAC Acak dengan HOTSPOT
+- Wireless MAC Authentication dengan User Manager 
+- Wireless Outdoor Mikrotik
+- TRAFFIC MONITOR MIKROTIK
+
+========================================
+2412-246xx chanel 1-6-11 20mhz 40mhz 1-11
