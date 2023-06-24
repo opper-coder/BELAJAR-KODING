13860c16f443b9ae2eddce4bebc68f9fd5cf6b25
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-24 17:28:42 +0800

    Update CATATAN.js

--- KODING/ANDROID/CATATAN.js
+++ KODING/ANDROID/CATATAN.js
@@ -3,7 +3,7 @@ TIPS HARI INI
 sepertinya belajar android studio itu mudah:
 1. belajarlah konsepnya saja
 2. desain gambar dari figma
-3. beli layout Sobakhul Munir Siroj. di figma community. 500rb an untuk template market place
+3. beli layout Sobakhul Munir Siroj. di figma community. 500rb an untuk template market place (sonysono1112 sudah follow) 
 4. bikin resource komponent reusable di android, juga layout seperti di nextjs
 5. Figma for VS Code: desain akan terintegrasi dengan vscode hasilnyha langsung copas
 6. cari yang untuk android studio
