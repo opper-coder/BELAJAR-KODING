cc864e25453a2aab715e9de9c0949d6b9d46a71d
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-06-24 15:39:43 +0800

    Update CATATAN.js

--- KODING/ANDROID/CATATAN.js
+++ KODING/ANDROID/CATATAN.js
@@ -1,1 +1,13 @@
-
+TIPS HARI INI
+------------------------------------------------
+sepertinya belajar android studio itu mudah:
+1. belajarlah konsepnya saja
+2. desain gambar dari figma
+3. bikin resource komponent reusable di android, juga layout seperti di nextjs
+4. konvert gambar di figma to android menggunakan export kit
+---
+kemudian tinggal atur: 
+- resource static komponent: text, warna, dll
+- lifecycle
+- cache
+- navigation dll 
