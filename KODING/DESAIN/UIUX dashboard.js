7b9e4cdd3604aec52b0f79fe8fb9b1553b82f912
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-03 15:03:46 +0800

    Create UIUX dashboard.js

---
+++ KODING/DESAIN/UIUX dashboard.js
@@ -0,0 +1,16 @@
+Navigasi
+1. session
+    - admin account
+    - root location
+    - tanggal
+    - paket kategori
+    - kategori
+    - list content
+2. navigasi session:
+    - Navigasi Items root
+3. Navigasi otorization (hidden/show,enable/disable)
+    - root
+    - superadmin
+    - admin
+    - seller
+    - enduser
