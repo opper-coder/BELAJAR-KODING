b862f798c2cc30ee0d9a9a26ecc32ae5d712358e
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-03 15:10:32 +0800

    Update UIUX dashboard.js

--- KODING/DESAIN/UIUX dashboard.js
+++ KODING/DESAIN/UIUX dashboard.js
@@ -1,4 +1,10 @@
-khusus untuk 
+khusus untuk mendesain dashboard dan juga aplikasi lainya
+muilai dulu dari 
+    1. menavigasikan session terlebih dahulu, 
+    2. hirarki admin outorization
+    3. konten item di halaman bersangkutan. adalah bagian akhir dari sebuah halaman
+    4. koneksikan dengan report atau rekapitulasi
+    5. kalau di butuh kan gunakan dashboard diagram, sbg langkah terakhir
 
 Navigasi
 1. session
@@ -7,11 +13,11 @@ Navigasi
     - periode/tanggal
     - paket kategori
     - list content
-2. navigasi session:
-    - Navigasi Items root
-3. Navigasi otorization (hidden/show,enable/disable)
+2. Navigasi otorization (hidden/show,enable/disable)
     - root
     - superadmin
     - admin
     - seller
     - enduser
+3. navigasi halaman:
+    - Navigasi Items
