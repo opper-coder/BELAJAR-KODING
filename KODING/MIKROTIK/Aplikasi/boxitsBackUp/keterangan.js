44ef287a26de3090009cb0fe2d3ab8e75565cb26
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-07 14:28:07 +0800

    Update keterangan.js

--- KODING/MIKROTIK/Aplikasi/boxitsBackUp/keterangan.js
+++ KODING/MIKROTIK/Aplikasi/boxitsBackUp/keterangan.js
@@ -1,1 +1,9 @@
-
+di dua folder ini saya ngoding boxit
+cara meletakkan adalah dengan cara:
+  persiapkan dulu project anda:
+  1. buat folder project
+  2. init nextjs > install node module yg di perlukan
+  3. node module > primereact
+  4. download sakai dan integrasikan
+  5. kopas folder boxitsComp di   :/root/boxitsComp
+  6. kopas folder boxits di       :/root/pages/boxits
