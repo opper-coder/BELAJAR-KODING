f6fef5b2a9a61f068d5b65b903fb573254491161
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-07 09:25:05 +0800

    Update OPER DATA.js

--- KODING/REACT JS/2023/OPER DATA.js
+++ KODING/REACT JS/2023/OPER DATA.js
@@ -1,3 +1,9 @@
 sumber youtube: 
 1. Semmi Verian > [NGE-React] #17 Mengirimkan data dari child ke parent component
 2. dea afrizal > Code Yang Harus React JS Developer Ketahui
+
+
+kirimdata dari paren ke child
+ const parent = () => {
+   
+ }
