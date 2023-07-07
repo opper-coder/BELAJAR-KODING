d6eb21879e96bb75e7750b446bd8e087f2c88a3c
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-07 09:31:57 +0800

    Update OPER DATA.js

--- KODING/REACT JS/2023/OPER DATA.js
+++ KODING/REACT JS/2023/OPER DATA.js
@@ -2,8 +2,37 @@ sumber youtube:
 1. Semmi Verian > [NGE-React] #17 Mengirimkan data dari child ke parent component
 2. dea afrizal > Code Yang Harus React JS Developer Ketahui
 
-
-kirimdata dari paren ke child
- const parent = () => {
-   
+kirimdata dari parent ke child
+------------------------------------------
+ const Parent = () => {                   // komponen parent nya
+  return
+  <>
+   <Child name="aqil">                    // panggil komponen dengan oper data ke child
+  </>  
  }
+ -----
+const Child = (props) => {                // komponen child tangkap data dengan props
+ return
+ <>
+  <div>Halo nama saya {props.name}</div>  // gunakan props
+ </>
+}
+
+kirimdata dari Child ke Parent
+------------------------------------------
+ const Parent = () => {
+  return
+  <>
+   <Child name="aqil"> 
+  </>  
+ }
+ -----
+const Child = (props) => {
+ return
+ <>
+  <div>Halo nama saya {props.name}</div>
+ </>
+}
+
+
+ 
