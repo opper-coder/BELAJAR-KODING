ac0543edaac768a1957d423b7c3e8f5b1ef69930
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-07 09:37:59 +0800

    Update OPER DATA.js

--- KODING/REACT JS/2023/OPER DATA.js
+++ KODING/REACT JS/2023/OPER DATA.js
@@ -4,30 +4,30 @@ sumber youtube:
 
 kirimdata dari parent ke child
 ------------------------------------------
- const Parent = () => {                   // 1. komponen parent nya
-  return
-  <>
-   <Child name="aqil">                    // 3. panggil komponen dengan oper data ke child
-  </>  
+ const Parent = () => {                    // 1. komponen parent nya
+ return
+    <>
+        <Child name="aqil">                // 3. panggil komponen dengan oper data ke child
+    </>  
  }
  -----
-const Child = (props) => {                // 2. komponen child tangkap data dengan props
+const Child = (props) => {                 // 2. komponen child tangkap data dengan props
  return
  <>
-  <div>"Halo nama saya "{props.name}</div>  // 4. gunakan props
+  <div>"Halo nama saya "{props.name}</div> // 4. gunakan props
  </>
 }
 
 kirimdata dari Child ke Parent
 ------------------------------------------
- const Parent = () => {                   // 1. parent 
+ const Parent = () => {                    // 1. parent 
   return
   <>
    <Child name="aqil"> 
   </>  
  }
  -----
-const Child = (props) => {                // 2. child
+const Child = (props) => {                 // 2. child
     const operData = () => {
         "halo yang ini nama saya"
     }
