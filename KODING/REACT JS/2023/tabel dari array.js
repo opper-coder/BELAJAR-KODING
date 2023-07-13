748185fe4ab6ee0ff1ebebcc372806dd227907ca
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-13 16:23:41 +0800

    Update tabel dari array.js

--- KODING/REACT JS/2023/tabel dari array.js
+++ KODING/REACT JS/2023/tabel dari array.js
@@ -1,1 +1,28 @@
+const data = [
+  { name: "Anom", age: 19, gender: "Male" },
+  { name: "Megha", age: 19, gender: "Female" },
+  { name: "Subham", age: 25, gender: "Male" },
+];
 
+function Coba() {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+}
