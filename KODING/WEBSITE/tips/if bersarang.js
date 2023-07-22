51d4aeba998e58b0ccbbfd1c3f7c9ad6443af59b
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-07-23 07:04:08 +0800

    Update if bersarang.js

--- KODING/WEBSITE/tips/if bersarang.js
+++ KODING/WEBSITE/tips/if bersarang.js
@@ -7,3 +7,8 @@ if(!true){                      // kondisi1 (semua pakai not agar mudah di jalan
       }else{ echo "kondisi3";}  // aksi kondisi3
     }else{ echo "kondisi2";}    // aksi kondis12
   }else{echo "kondisi1";}       // aksi kondis11
+
+tips nya:
+1. bikin algoritma
+2. bikin semua if secara mandiri
+3. bikin kerangka bersarang 3, 4, 5 dst (di atas) > lalu tempatkan ke posisinya masing2 
