85e267b5690a7b21fe4113fc33c89dc4319f9a21
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-06 20:28:32 +0800

    Create i3-config-dasar3.js

---
+++ KODING/MIKROTIK/basic-config/i3-config-dasar3.js
@@ -0,0 +1,47 @@
+setting dasar mikrotik
+
+---------------------------------------
+RESET
+- Reset mikrotik 
+- login
+- biarkan setting default
+- reset dari system, no backup, no default config
+---------------------------------------
+BRIDGE-IP
+- bridge-WAN (port 1)
+- bridge-LAN (semua port kecuali terkhir)
+- bridge-speedtest (port terakhir)
+- add ip bridgelan 192.168.50.1/24
+- add ip bridge-speedtest 192.168.52.1/24
+---------------------------------------
+DHCP CLIENT
+- ADD > bound
+- add NAT masquerade
+- add DNS 8888, 8844, allow 
+- :> ping google.com 
+---------------------------------------
+HOTSPOT
+- hopspot server hotspot-setup > next2
+- dns: halaman login local: ITS.net
+- login pertama "admin", "admin1234"
+---------------------------------------
+SETUP TAMBAHAN
+---
+- hospot>server>dblc>
+- addresspool: none, idle timeout:5 menit, keepalive: 6 menit, adrees per mac:1, 
+	tab login: HTTP CHAP,HTTP PAP,MAC COOKIE=true, other:no
+---
+- DHCP server > dblc > lease-time: 10 mnt > add
+---
+system > clock
+> system zone auto detect: matikan
+> system SNTP client > enable > primary NTP : 202.65.114.202 > secondary NTP : 212.26.18.41 > server dns: "asia.pool.ntp.org"
+> apply maka mode akan berubah beserta parameter lain 
+---------------------------------------
+SECURITY
+- sistem user > add user > grup full > name: its.net > pass: pasmikro > lalu login ulang dg admin baru > kemudian disable yang bawaan   
+- system identity > ITS.net >  
+---------------------------------------
+loginpage
+-> files > hapus smua kecuali hotspot lalu ambil login page milik anda paste di "dalam" folder /hotspot
+
