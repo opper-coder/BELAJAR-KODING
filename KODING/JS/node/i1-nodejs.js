2c908d5a00d51a917d1f636cad6d977a618a36cc
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-08 14:22:14 +0800

    Update i1-nodejs.js

--- KODING/JS/node/i1-nodejs.js
+++ KODING/JS/node/i1-nodejs.js
@@ -1,1 +1,138 @@
+Node js
+------------------------------------------------- 
+INSTALASI
+REPL
+INDEX.JS
+SINGLE PAGE APP
+MODULES
+EXPORT
+IMPORT
+CORE MODULES
 
+-------------------------------------------------
+INSTALASI
+:> node -v 		: 
+:> npm -v 		: 
+:> mkdir belajar-node 	: add
+:> cd belajar-node 	: masuk
+----------------
+REPL
+:> node 		: masuk ke REPL (read, eval, print, loop)
+:> 1+1 atau 1=='1' enter: mirip console pada browser
+:> .load coba.js 	: import file dalam repl
+:> .save coba2.js 	: simpan code repl ke file
+:> .exit / ctrl C, C 	: keluar repl
+:> .break 		: keluar dari code multiline di repl (karena nggak bisa keluar begitu saja nanti kode bisa hilang)
+:> .editor 		: nulis multiline, 
+:> ctrl D 		: finis multiline,
+:> ctrl c 		: cancel multiline,
+----------------
+INDEX,JS
+:> mkdir belajar-node 	: add
+:> cd belajar-node 	: masuk
+:> code . 		: 
+-> new index.js > console.log("hello world")
+:> node index.js 	: jalankan file di runtime node atau
+:> node . 		: file index.js ini (ingat tidak bisa manggil index.html)
+-> index.html 		: (html biasa itu client) mimiliki script src=coba.js dan coba2.js itu bisa di lakukan tapi bisa di akses secara global dan client
+----------------
+SINGLE PAGE APP 
+:> index.js 		: satu file index, akan meng include semua page lainya sedangkan element html akan di generate via js dom 
+ 			  sehingga seluruhnya file javascript tidak ada lagi file html, sehingga tidak global lagi tidak client lagi, melainkan local dan server
+-> require('./coba') 	: sejajar ../ di atasnya ../../ di atasnya dua kali './folder1/coba' dalam folder1 sejajar
+-> module.export=salam; : halaman bisa di include, tapi isinya tidak bisa di pakai kecuali yg di export,
+-> const salam = require('./coba') : pada prakteknya harus di bungkus dalam var, lalu var bisa di gunakan menggunakan "require" atau di npm pakai import 
+   			  (nanti di npm init, package.json "type":"module").
+----------------			  
+MODULES
+sekumpulan code reusable dengan antarmuka yg terdefinisi, atau
+sebuah fungsi komplex/simpel dalam file javascript, yang di export, dan bisa di gunakan(require) di file.js lain dalam nodejs tersebut 
+1 core module 		: packet dalam node tidak terlihat (dalanm library) kita tinggal require
+2 local module 		: module yang kita bikin sendiri "export require"
+3 third party 		: module pihak ketiga atau "modul npm" (kumpulan module yg di koleksi oleh node)
+---
+const fs =require('fs') : tanpa url, maka akan cari di coremodule, jika tdk ada lalu di local, jk tdk ada maka di third party module
+			  gampang nya kalau tanpa url berarti core atau third, sedang url berarti local
+----------------	  
+EXPORT
+- var, arr, obj, func, class, 	: yg umum di export 
+- module.export=salam; 	: export sebuah function dalam sebuah file.js
+1. module.export.salam=salam : 
+2. module.export.halo=halo : export beberapa(dua) module dalam sebuah file.js. (put dalam object) 
+atau export langsung dalam bentuk object
+module.export={
+	salam:salam,
+	halo:halo,
+}
+atau short hand karena key value sama 
+module.export={ salam, halo }
+----------------
+IMPORT
+- const salam= require('./coba') : import satu module dalam sebuah file coba.js
+- const coba= require('./coba')  : file memiliki beberapa(dua) module diexport
+- console.log(coba.nama) 	 : kalau di export dalam objkect kita bisa panggil 
+- import * as fs from 'node:fs'; : node modern sudah bisa import cekidoc 
+
+-------------------------------------------------
+CORE MODULES
+-> go https://nodejs.org/dist/latest-v18.x/docs/api/
+> terlihat list core module  
+:> mkdir belajar-node 	: add
+:> cd belajar-node 	: masuk
+:> code . 		: 
+-> new app.js 		: bikin entry point, > lalu coba salh satu modul (fs: file system)
+	fs 		: module untuk CRUD file, menulis isi file dll banyak method di dalmnya termasuk CRUD folder juga
+	const fs = require('node:fs');				: import dulu
+	fs.writeFileSync("test.txt", "Hallo world sync"); 	: (SYNC) jalankan method nya( create file sekaligus menulis isinya, secara sync )
+:> ls 			: cek ada file baru "test.txt"
+	fs.Promises.mkdir(path[, options])			: coba lagi fs.mkdir bikin direktori 
+> try{}catch(e){} 	: kalau pakai sync pada prakteknya kita harus tangkap jalan tidaknya perintah pakai try catch 
+> node app 		: jalankan app 
+	fs.writeFile("test2.txt", "Hallo world sync", (err) => {
+		if(err) throw err; {
+			console.log(err);
+		}
+	})
+
+setidaknya kita telah belajar 
+1. create file sekaligus mengisi filenya dengan : fs.writeFileSync()
+2. mengisi existing file dengan string dan json dengan form input terminal : fs.Promises.mkdir() fs.writeFile() dll
+
+-------------------------------------------------
+NPM MODULES
+:> mkdir belajar-node 	: add
+:> cd belajar-node 	: masuk
+:> touch app.js 	: add console.log("hello")
+:> node app 		: jalankan app.js, retun "hello"
+:> npm init 		: wait finish
+-> package.json 	: cek file package
+> script:{} 		: disitu k:v jika di isi bisa di panggil pakai npm 
+-> {"start":"node app"} : masukan k:v ini, 
+:> npm start 		: jalankan node app di npm pada key script:{}, key start, test adalah script standard 
+:> npm run dev 		: kalau bikin sendiri harus pakai "run"
+-> search di npm web 	: instalasi package, sebelum instalasi search package dulu di web npm
+-> validator 		: misalnya "validator", copas import nya ES6/ES
+:> npm i validator 	: jk ditemukan, instalasi akan di berikan yg terbaru, wait finish
+:> npm i validator@13.5.2 	: instalasi pakai version "@", wait finish. cek versi di "npmjs.com"
+-> package.json 	: cek version.
+-> node_modules/ 	: ada tambahan folder berisi validator, dan node_modules default lainya
+-> npm uninstall validator 	: hapus package
+:> npm install -g nodemon # or using yarn: yarn global add nodemon : salah satu package npm untuk perubahan saat di save, langsung realtime
+-------------------------------------------------
+cara pakai nya 
+-> app.js 		: edit file berisI:
+ 	var validator = require('validator'); 			// import gaya lama (untuk sanitasi input)
+ 	validator.isEmail('foo@bar.com'); 			// => true, penggunaan 
+	---
+ 	import chalk from 'chalk'; 				// import gaya baru (untuk pewarnaan console)
+	console.log(chalk.blue('Hello world!'));  		// penggunaan helloworld akan berwarna biru di console
+	---
+	nodemon 						: jika kita mau app realtime berarti hrs di install scr local dg mengilangkan -g
+	ctrl C 							: keluar nodemon 
+	npm uninstall -g nodemon  				: uninstall global 
+	npm i --save-dev nodemon@1.1.0				: install local
+	-> script{ "start":"nodemon app.js" }			: nodemon app tidak jalan kecuali di simpan dalam script:{}
+	:> npm start
+
+
+-------------------------------------------------
