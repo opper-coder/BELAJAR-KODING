c10148d9094d6df31d2f8da4f771df97685c68be
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-10 07:56:05 +0800

    Create kalibrasi.js

---
+++ KODING/MIKROTIK/splicer/kalibrasi.js
@@ -0,0 +1,10 @@
+----------------------------------------------------
+kalibrasi di perlukan 
+
+
+1. download signalfire2
+2. sambungkan bluetooth
+3. buka cover
+- pasang
+
+----------------------------------------------------
