a729a10bedb388a0e32c0b0c6209921cb10e1bc9
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-10 09:13:09 +0800

    Update kalibrasi.js

--- KODING/MIKROTIK/splicer/kalibrasi.js
+++ KODING/MIKROTIK/splicer/kalibrasi.js
@@ -1,17 +1,43 @@
+GANTI ELEKTRODA 
 ----------------------------------------------------
-KALIBRASI
+- alasan elektrotoda di ganti: adalah counternya sudah habis, tungsten nya sudah rusak, dll
+- buka elektroda lama, siapkan yng baru harga: 800rb - 1,2jt 
+- buka baut > lepaskan led infrared > dan ganti > jangan lupa baut harus hati2 
+
+----------------------------------------------------
+AKTIFASI ELEKTRODA
+- alasan aktifasi
+
+1. download signalfire2 android
+2. sambungkan bluetooth dengan splicer (pastikan jangan salah perangkat)
+3. activate electroda > serial number (atau scan barcode) elektroda > verification code > 
+   > maka remain akan di reset ke 3000
+
+----------------------------------------------------
+KALIBRASI POWER
 - kalibrasi di perlukan sebaiknya setiap akan melakukan penyambungan  
 - hal ini di perlukan karena perbedaan daya charge, panjang katoda, dan perbedaan suhu indoor outdoor serta cuaca 
 - saat kalibrasi akan ada pembakaran saat over power, pembakaran akan mundur dan membulat, 
   software akan melakukan kalkulasi dan menentukan power yang cocok 
   
-1. download signalfire2
-2. sambungkan bluetooth
+1. download signalfire2 android
+2. sambungkan bluetooth dengan splicer (pastikan jangan salah perangkat)
 3. buka cover
 4. pasang kabel seperti penyambungan biasa 
 5. koneksi bluetooth
-  > fiber splicing set > running mode > ARC calibration => layar berubah jadi ada tanda plus
+  > fiber splicing set(set penyambungan serat) > running mode > ARC calibration => layar berubah jadi ada tanda plus
   > siapkan kabel bersihkan dan potong (seperti penyambungan biasa)
   > lakukan 2 - 3 kali, sampai ada notif "kalibrasi sukses" > restart splicer  
   > selesai 
 ----------------------------------------------------
+KALIBRASI KAMERA
+- bisa saja kamera blur atau posisi tidak di tengah, ini bukan kesalahan hardware, 
+- kita bisa membetulkanya dengan setting kalibrasi
+
+2. sambungkan bluetooth dengan splicer (pastikan jangan salah perangkat)
+3. buka cover
+4. pasang kabel seperti penyambungan biasa 
+5. koneksi bluetooth
+  > siapkan kabel bersihkan dan potong (seperti penyambungan biasa)
+  > fiber splicing set(set penyambungan serat) > manual > kalibrasi sumbu x dan z > finish > selesai
+  > selesai 
