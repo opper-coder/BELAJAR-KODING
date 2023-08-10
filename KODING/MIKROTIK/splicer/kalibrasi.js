4d621c194a66442d2d9e53c1eb5342dbec405a0f
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-10 08:44:04 +0800

    Update kalibrasi.js

--- KODING/MIKROTIK/splicer/kalibrasi.js
+++ KODING/MIKROTIK/splicer/kalibrasi.js
@@ -1,10 +1,17 @@
 ----------------------------------------------------
-kalibrasi di perlukan 
-
-
+KALIBRASI
+- kalibrasi di perlukan sebaiknya setiap akan melakukan penyambungan  
+- hal ini di perlukan karena perbedaan daya charge, panjang katoda, dan perbedaan suhu indoor outdoor serta cuaca 
+- saat kalibrasi akan ada pembakaran saat over power, pembakaran akan mundur dan membulat, 
+  software akan melakukan kalkulasi dan menentukan power yang cocok 
+  
 1. download signalfire2
 2. sambungkan bluetooth
 3. buka cover
-- pasang
-
+4. pasang kabel seperti penyambungan biasa 
+5. koneksi bluetooth
+  > fiber splicing set > running mode > ARC calibration => layar berubah jadi ada tanda plus
+  > siapkan kabel bersihkan dan potong (seperti penyambungan biasa)
+  > lakukan 2 - 3 kali, sampai ada notif "kalibrasi sukses" > restart splicer  
+  > selesai 
 ----------------------------------------------------
