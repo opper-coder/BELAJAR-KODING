4f6600e1ecedbbd583ac27fe33b321d8f3a6995b
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-22 06:55:21 +0800

    Update i3-config-dasar3.js

--- KODING/MIKROTIK/basic-config/i3-config-dasar3.js
+++ KODING/MIKROTIK/basic-config/i3-config-dasar3.js
@@ -6,6 +6,22 @@ RESET
 - biarkan setting default
 - reset dari system, no backup, no default config
 ---------------------------------------
+SECURITY
+- saran dilakukan di awal, dan pastikan setting IP private sudah bisa di akses dilocal atau webfig
+- sistem user > add user > grup full > name: its.net > pass: pasmikro > lalu login ulang dg admin baru > kemudian disable yang bawaan   
+- system > identity > ITS.net >  
+- IP > services > ada list > disable dan sisakan 'www, winbox, ssh'
+  - dklik u/ config ssh > available from isikan ip target 
+    public dari DDNS misalnya,202.11.1.1 -lalu tambahkan IP local kita 192.168.50.0/24 lalu tambahkan ip statik spesifik 192.168.50.5
+  - dclick winbox > available from isikan   192.168.50.0/24, lalu tambahkan  192.168.50.5 
+  - Terus www > edit port 80 kita ubah jadi 8080 > Nanti kalau remote tinggal www.ip:8080 
+- Tips yang ini jangan dilakukan terlalu cepat nanti tidak bisa connect sendiri
+Langkah amanya tiap perubahan ping dst
+---------------------------------------
+WEB FIG
+http://ip:8080
+???
+---------------------------------------
 BRIDGE-IP
 - bridge-WAN (port 1)
 - bridge-LAN (semua port kecuali terakhir)
@@ -36,17 +52,7 @@ system > clock
 > system zone auto detect: matikan
 > system SNTP client > enable > primary NTP : 202.65.114.202 > secondary NTP : 212.26.18.41 > server dns: "asia.pool.ntp.org"
 > apply maka mode akan berubah beserta parameter lain 
----------------------------------------
-SECURITY
-- sistem user > add user > grup full > name: its.net > pass: pasmikro > lalu login ulang dg admin baru > kemudian disable yang bawaan   
-- system > identity > ITS.net >  
-- IP > services > ada list > disable dan sisakan 'www, winbox, ssh'
-  - dklik u/ config ssh > available from isikan ip target 
-	public dari DDNS misalnya,202.11.1.1 -lalu tambahkan IP local kita 192.168.50.0/24 lalu tambahkan  192.168.50.5
-  - dclick winbox > available from isikan   192.168.50.0/24, lalu tambahkan  192.168.50.5 
-  - Terus www > edit port 80 kita ubah jadi 8080 > Nanti kalau remote tinggal www.ip:8080 
-- Tips yang ini jangan dilakukan terlalu cepat nanti tidak bisa connect sendiri
-Langkah amanya tiap perubahan ping dst
+
 ---------------------------------------
 loginpage
 -> files > hapus smua kecuali hotspot lalu ambil login page milik anda paste di "dalam" folder /hotspot
