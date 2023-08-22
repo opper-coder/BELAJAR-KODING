a8f96ac713a151de189c0717835f8dff84328f3a
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-22 13:50:52 +0800

    Update i1-persiapan.js

--- KODING/firebase/NEXT-FIREBASE/i1-persiapan.js
+++ KODING/firebase/NEXT-FIREBASE/i1-persiapan.js
@@ -75,7 +75,7 @@ import { 										// 0. setelah config inisialisasi awal maka kita butuh bebera
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN, 
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
