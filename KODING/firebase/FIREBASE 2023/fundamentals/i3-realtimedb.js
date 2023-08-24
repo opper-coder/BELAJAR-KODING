cf2fee4c2090b60c0fa4b104fe6fc2f40e103bc6
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-24 18:16:46 +0800

    Update i3-realtimedb.js

--- KODING/firebase/FIREBASE 2023/fundamentals/i3-realtimedb.js
+++ KODING/firebase/FIREBASE 2023/fundamentals/i3-realtimedb.js
@@ -1,1 +1,38 @@
+-------------------------------------------------
+DASHBOARD FIREBASE
+
+-> console firebase 
+-> pilih atau add project
+-> aktifasi realtimedb
+-> next2 finish
+-> mirip json kita harus bikin dokument sebagai tabel lalu kolom sebagai field (ikuti best practice kedalaman nested ??> )
+-------------------------------------------------
+STRUCTUR PROJECT
+ > kita akan gunakan instalasi dan inisialisasi pada project i1 dan i2 
+ >   
+
+-------------------------------------------------
+        import { initializeApp } from 'firebase/app'                                    
+        import { getDatabase, ref, set} from 'firebase/database'
+
+        const firebaseApp = initializeApp({                                             
+          // ...
+        })
+
+        const tambahUser = (userId, name, email, imageUrl) => {
+          const db = getDatabase();                               
+          const reference = ref(db, 'user/' + userId);            
+                                                                  
+          set(reference, {                                        
+            username: name,
+            email: email,
+            profile_picture: imageUrl
+          })
+        }
+
+        tambahUser("zikri aqil", "aqil", "aqil@coba.com", "myImgUrl");  : invoke tambahUser() akan menulis data di database dan bersifat replace jika ada
+                                                                          maka berfungsi juga sebagai "create" dan "edit"
+-------------------------------------------------
+        :> "serve src/"                                         : jalankan kode 
+        -> cek di dashboard database                            : maka data akan bertambah jika belum ada
 
