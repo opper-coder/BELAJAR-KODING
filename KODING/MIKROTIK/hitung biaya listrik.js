430d12ee1317785595a9d82a1a502cefa6a33dda
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-10-30 05:30:17 +0800

    Update hitung biaya listrik.js

--- KODING/MIKROTIK/hitung biaya listrik.js
+++ KODING/MIKROTIK/hitung biaya listrik.js
@@ -1,5 +1,5 @@
-1. whatt x jam x harg per kwh           = menghasilkan rupiah
-2. 12volt x 2amp x jam x hrga per kwh  = menghasilkan rupiah
+1. whatt(v * a) x jam(24 jam) x harg per kwh x hari      = menghasilkan rupiah
+2. 12volt x 2amp x jam x hari (30hari) x hrga per kwh    = menghasilkan rupiah
 
 harga per Kwh tidak sama R1, R2, R3 dst paket 450w, 900w, 1300w
 perkiraan hitungan router per bulan pada meteran 450w r1
