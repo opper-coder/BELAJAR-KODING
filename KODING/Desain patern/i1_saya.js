3e2436fd3c04c1d954cffee4360b4d442670064d
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-11-27 17:04:40 +0800

    Update i1_saya.js

--- KODING/Desain patern/i1_saya.js
+++ KODING/Desain patern/i1_saya.js
@@ -2,10 +2,32 @@ Problem Solving:
 1. definisikan judul masalah
 2. bikin kategori 
 3. breakdown kategory:
-    - entity        :
-    - aktifity        :
-    - fungsi        :
-    - koneksi        :
-    - indexing        :        
+    - entity (daftar objek):
+        - product
+        - pihak
+        - jumlah
+    - aktifity (daftar peristiwa)
+        - aksi
+        - reaksi
+        - waktu
+    - fungsi (daftar fungsi)
+        - eksekusi
+            - CRUD
+        - evaluasi (properties, table, realtime)
+            - monitoring
+            - informasi
+        - solusi (prosedur)
+            - error handling
+        - hasil (logic)
+            - data permanent
+            - data cache
+    - koneksi (daftar hubungan)
+        - db relational
+        - api spesification
+        - paging
+        - 
+    - indexing (urutan logic peristiwa)
+        - happy flow
+        - sad flow
 4. page desain
 5. 
