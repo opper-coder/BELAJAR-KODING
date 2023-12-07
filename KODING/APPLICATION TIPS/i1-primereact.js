18792f6435bc5f82c5f0d3648a919976bf84e6fb
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-08 02:09:00 +0800

    Create i1-primereact.js

---
+++ KODING/APPLICATION TIPS/i1-primereact.js
@@ -0,0 +1,38 @@
+TIPS PAKAI PRIMEREACT
+--------------------------------------------------------
+1. bikin satu folder app 
+2. pecah folder di dalamnya sesuai service kita
+3. bikin masing2 service mandiri tanpa ada keterikatan dengan lainya sebisa mungkin
+4. gunakan layout dan thema dari sakai
+5. siap di gunakan tinggal menguasai sedikit tentang komponen
+
+TABEL
+--------------------------------------------------------
+tabel pada komponen ada dua type
+1. tabel yang data nya kita sediakan sorting dan filternya
+2. tabel yang menyediakan sorting dan filter 
+
+BACKEND
+--------------------------------------------------------
+kita tinggal membuatkan bebrapa fungsi dasar
+1. fungsi pemanggil db yang dapat di filter dengan object, 
+	pemanggilya mengirimkan filter dan sumber data secara async
+	yang return nya object yang sudah terfilter
+2. fungsi mengambil data terakhir sesuai tanggal
+3. fungsi konversi dr obj ke array
+4. fungsi summary array
+5. fungsi itu dapat menampilkan data tunggal atau jamak seperti tabel, atau infosaldo
+6. kita dah punya ya itu tinggal pakai
+
+TIPS MEMBUAT APP DASHBOARD
+--------------------------------------------------------
+- bagi semua service ke dalam alur alur nya
+- setiap alur memiliki beberapa page
+- breakdown fungsi page 
+	- data tunggal
+	- data jamak
+	- action/button
+	- modals
+- lakukan pada masing2 page 
+
+DATA BASE
