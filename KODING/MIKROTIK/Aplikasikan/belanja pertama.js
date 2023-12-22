f747828a2af25ec71d2a1b0d694e3e17960cf715
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-22 09:21:18 +0800

    Create belanja pertama.js

---
+++ KODING/MIKROTIK/Aplikasikan/belanja pertama.js
@@ -0,0 +1,14 @@
+- RB450 gr3     : 1 jt
+- OLT           : 4 jt
+- Splitter      : 8 x 70rb
+- ODP           : 8 x 150rb
+- kabel         : 6 km x 800
+- router        : 40 x 300rb
+
+- tangga pendek 4m     : 3jt
+- teleskopik pole 4m 2 bh  : 400rb
+- OTDR                 : 4jt
+- OPM                  : 200rb
+- voltase meter        : 2 buah 
+- watt meter digital   : 1 buah
+- 
