dd9db3976854e86790b5b48abf4a9ca029a5996c
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-23 21:18:00 +0800

    Create hotspot-cepat-diawal.js

---
+++ KODING/MIKROTIK/Aplikasikan/hotspot-cepat-diawal.js
@@ -0,0 +1,69 @@
+apakah batasan seperti di atas bisa dilakukan dengan menggunakan simple queue semua
+ChatGPT
+Ya, Anda bisa menggunakan Simple Queue untuk mencapai tujuan yang Anda inginkan tanpa perlu menggunakan Queue Tree. Anda dapat membuat aturan-aturan Simple Queue yang lebih kompleks untuk mencakup pembatasan kecepatan pada interval waktu yang berbeda.
+
+Berikut contoh penggunaan Simple Queue untuk batasan kecepatan selama 3 menit pertama yang kemudian berubah menjadi kecepatan yang lebih lambat:
+
+Langkah 1: Membuat Simple Queue untuk batasan kecepatan
+Buat Simple Queue untuk kecepatan cepat selama 3 menit:
+
+arduino
+Copy code
+/queue simple
+add name=fast-limit target=192.168.1.100/32 max-limit=5M/5M burst-limit=10M/10M burst-threshold=6M/6M burst-time=10s/10s 
+Ganti 192.168.1.100 dengan alamat IP yang ingin Anda batasi kecepatannya.
+
+Buat skrip untuk mengubah kecepatan setelah 3 menit:
+
+bash
+Copy code
+/system script
+add name=change-speed-policy policy=ftp,reboot,read,write,policy,test,winbox,password,sniff,sensitive,api source="/queue simple set [find name=fast-limit] max-limit=1M/1M"
+Skrip ini akan mengubah batasan kecepatan pada Simple Queue yang telah dibuat setelah 3 menit berlalu.
+
+Atur penjadwalan untuk menjalankan skrip setelah 3 menit:
+
+sql
+Copy code
+/system scheduler
+add interval=3m name=change-speed-scheduler on-event=change-speed-policy start-time=startup
+Dalam contoh ini, Simple Queue digunakan untuk mengatur batasan kecepatan awal dan setelah 3 menit, skrip dijadwalkan untuk menjalankan perubahan kecepatan pada Simple Queue yang sudah ada. Pastikan untuk menyesuaikan alamat IP dan kebutuhan kecepatan sesuai dengan jaringan Anda.
+
+User
+pada contoh diatas simple queue ini di terapkan pada user profile hotspot apa bisa? 
+ChatGPT
+Ya, Anda bisa menerapkan aturan Simple Queue pada User Profile Hotspot di MikroTik. Ini memungkinkan Anda untuk secara efektif menerapkan pembatasan kecepatan kepada pengguna yang terhubung melalui profil Hotspot tertentu.
+
+Berikut contoh bagaimana Anda bisa menerapkan Simple Queue pada User Profile Hotspot:
+
+Langkah 1: Membuat Simple Queue untuk batasan kecepatan
+Buat Simple Queue untuk kecepatan cepat selama 3 menit:
+
+arduino
+Copy code
+/queue simple
+add name=fast-limit target=192.168.1.100/32 max-limit=5M/5M burst-limit=10M/10M burst-threshold=6M/6M burst-time=10s/10s 
+Ganti 192.168.1.100 dengan alamat IP atau rentang IP pengguna Hotspot yang ingin Anda batasi kecepatannya.
+
+Langkah 2: Terapkan Simple Queue pada User Profile Hotspot
+Edit User Profile Hotspot:
+
+Anda dapat menambahkan aturan Simple Queue ke dalam User Profile Hotspot.
+
+arduino
+Copy code
+/ip hotspot user profile
+set [find name=default] rate-limit=fast-limit
+rate-limit=fast-limit mengacu pada nama Simple Queue yang telah Anda buat sebelumnya. Ini akan mengaitkan aturan Simple Queue dengan profil pengguna hotspot.
+
+Langkah 3: Otomatisasi Perubahan Kecepatan
+Buat skrip dan jadwal untuk mengubah kecepatan setelah 3 menit:
+
+Langkah ini tetap sama seperti yang sudah dijelaskan sebelumnya dalam contoh sebelumnya. Skrip akan dijadwalkan untuk menjalankan perubahan kecepatan pada Simple Queue yang sudah ada setelah 3 menit.
+
+Pastikan untuk menyesuaikan alamat IP, nama Simple Queue, dan kebutuhan kecepatan sesuai dengan jaringan Anda. Dengan melakukan ini, aturan kecepatan akan diterapkan pada pengguna hotspot berdasarkan profil yang mereka gunakan.
+
+
+
+
+
