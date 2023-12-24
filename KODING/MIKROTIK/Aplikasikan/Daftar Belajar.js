0df41e946b8672d02c71739e050f19fef5ca2494
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-24 18:19:16 +0800

    Create Daftar Belajar.js

---
+++ KODING/MIKROTIK/Aplikasikan/Daftar Belajar.js
@@ -0,0 +1,22 @@
+daftar yang harus di pelajari untuk standard RT/RW
+--------------------------------------------------
+standard 
+1. keamanan
+2. konfig dasar
+3. burst standart
+4. kencang di 3 menit pertama
+5. user profil 1,2,3,4,5 mbps
+6. restart scheduling mikrotik, OLT, aksespoint 3 hari sekali 
+7. hapus sampah voucher berkala 2 bulan terakhir terpakai otomatis dan sudah ada pencairan
+
+--------------------------------------------------
+monitoring
+- speedtest mikrotik jarak jauh
+- speedtest aksespoint jarak jauh
+- enable/disable mikrotik
+- enable/disable aksespoint
+- disable/enable OLT
+- lambat 
+- deteksi mati titik (lokasi)
+- deteksi putus kabel (lokasi)
+- 
