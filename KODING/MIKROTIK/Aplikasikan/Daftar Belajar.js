255d6326653a9b5d15ebc74fc281d7818de885c8
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-25 06:01:19 +0800

    Update Daftar Belajar.js

--- KODING/MIKROTIK/Aplikasikan/Daftar Belajar.js
+++ KODING/MIKROTIK/Aplikasikan/Daftar Belajar.js
@@ -9,7 +9,6 @@ standard
 6. restart scheduling mikrotik, OLT, aksespoint 3 hari sekali 
 7. hapus sampah voucher berkala 2 bulan terakhir terpakai otomatis dan sudah ada pencairan
 8. the dude
-
 --------------------------------------------------
 monitoring
 - speedtest mikrotik jarak jauh
@@ -20,7 +19,6 @@ monitoring
 - lambat 
 - deteksi mati titik (lokasi)
 - deteksi putus kabel (lokasi)
-- 
 --------------------------------------------------
 THE DUDE
 - the dude server CHR
@@ -35,24 +33,41 @@ THE DUDE
 NETWATCH
 - monitoring akses point menggunakan telegram
 - melihat mati atau lambat sebuah akses point lokal
-
 --------------------------------------------------
 KOMBINASI
-- the dude memiliki cara monitoring lebih lengkap dan banyak di server cloud CHR
+THE DUDE
 - kelebihan:
+  - the dude memiliki cara monitoring lebih lengkap dan banyak di server cloud CHR
   - bisa mementau secara masal dan grup monitoring
   - bisa di hidupkan saat di gunakan saja
   - memiliki i8nformasi yang lengkap
   - semua perangkat yang memiliki IP bisa di monitoring bahkan IoT selain mikrotik
   - memiliki datasheet informasi lengkap terhadap perangkat bersangkutan
-  - 
 - kekurangan:
   - harus pakai komputer
   - jika VPN terganggu maka akan terlihat seperti putus pada perangkat bersangkutan
   - membayar hosting
   - memerlukan beberapa VPN cadangan, baik untuk dedude ataupun untuk VPN perangkat
+NETWHATCH
+- kelebihan
+  - langsung bisa di jalankan di mikrotik bersangkutan
+  - tidak memerlukan vpn
+  - langsung dikirim via telegram HP
+  - akurat dan direct untuk informasi pertama
+  - selanjutnya di periksa di the dude
   - 
+--------------------------------------------------
 
 
 
-  
+
+
+
+
+
+
+
+
+
+
+
