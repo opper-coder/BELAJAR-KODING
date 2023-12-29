b206eabcab22c8235a2a9f322ff2c9cad10cb111
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-30 05:02:17 +0800

    Update i2-CHR.js

--- KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i2-CHR.js
+++ KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i2-CHR.js
@@ -5,8 +5,16 @@ CHR
 - bikin lingkungan bandwidth test mikrotik
 - bikin VPN untuk serverlocal yang akan terhubung ke DUDE dan BOXITS 
 -----------------------------------------------------------------------
-
-
-
-  
+- hosting pakai kenceng solusindo
+  - mikrotik 150rb, dedicated
+  - nodejs 30rb
+  - lokasi jakarta, manado,(pilih manado)
+  - lumayan terkenal 
+  - pembayaran lewat VA
+  - uptime 99%
 -----------------------------------------------------------------------
+config dude CHR
+  
+
+
+
