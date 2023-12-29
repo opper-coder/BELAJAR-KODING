348e20e2891bd0ba83846f12d3a1ebce1b2f266f
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-29 21:17:58 +0800

    Create i1-konsep.js

---
+++ KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i1-konsep.js
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------
+Konfig terapan untuk bisnis ini
+
+1. bikin lingkungan monitoring dude
+2. bikin lingkungan monitoring telegram
+3. bikin VPN untuk server yang akan terhubung ke monitoring dan login voucer 
+4. boxits 
+5. bikin lingkungan login page redirect ke server 
+6. bikin lingkungan boxit dengan management mobile
+
