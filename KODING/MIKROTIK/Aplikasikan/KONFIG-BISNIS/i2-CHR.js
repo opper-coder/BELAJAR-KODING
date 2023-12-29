66d91d573f8ff9192d6e27d19b27c38ec7614ae4
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-30 04:11:13 +0800

    Update i2-CHR.js

--- KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i2-CHR.js
+++ KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i2-CHR.js
@@ -1,12 +1,9 @@
 -----------------------------------------------------------------------
 CHR
 - hosting dimana
-- konfignya gimana
-- semua fitur ada
-- siap pakai konfignya pokoknya
-  - bikin lingkungan monitoring dude
-  - bikin lingkungan bandwidth test mikrotik
-  - bikin VPN untuk serverlocal yang akan terhubung ke DUDE dan BOXITS 
+- bikin lingkungan monitoring dude
+- bikin lingkungan bandwidth test mikrotik
+- bikin VPN untuk serverlocal yang akan terhubung ke DUDE dan BOXITS 
 -----------------------------------------------------------------------
 
 
