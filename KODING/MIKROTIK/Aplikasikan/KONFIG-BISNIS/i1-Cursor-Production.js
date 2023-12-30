224aa9c0798fde65f3721224ec92a90c1cdaba20
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-12-30 15:10:11 +0800

    Update i1-Cursor-Production.js

--- KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i1-Cursor-Production.js
+++ KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i1-Cursor-Production.js
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------
 Catatan 
 	CHR
-		* bikin lingkungan monitoring dude
+		** bikin lingkungan monitoring dude
 		- bikin VPN untuk serverlocal yang akan terhubung ke DUDE dan BOXITS 
 		- pertimbangkan VPN atau Tunnel (mengingat ini untuk monitoring dan eksekusi aktifkan voucher)
 		- speedtest CHR - local
