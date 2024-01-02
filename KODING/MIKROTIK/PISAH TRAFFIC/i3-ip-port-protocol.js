6e8b82ec2c60ac280ccb98a7622ddf84353ab069
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-01-02 16:51:49 +0800

    Update i3-ip-port-protocol.js

--- KODING/MIKROTIK/PISAH TRAFFIC/i3-ip-port-protocol.js
+++ KODING/MIKROTIK/PISAH TRAFFIC/i3-ip-port-protocol.js
@@ -1,1 +1,11 @@
 cari bagaimana cara mendapatkanya
+---------------------------------------------------- 
+CARI PORT GAME
+  di nanakmrk
+port game
+  mobile legend
+  freefire
+  pubg mobile
+  dota
+  coc
+  high domino
