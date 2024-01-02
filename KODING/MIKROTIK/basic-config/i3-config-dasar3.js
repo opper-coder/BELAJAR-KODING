0f2afa64fffb2221c51ab7940357fbdcca9c2cfe
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-01-02 09:00:50 +0800

    Update i3-config-dasar3.js

--- KODING/MIKROTIK/basic-config/i3-config-dasar3.js
+++ KODING/MIKROTIK/basic-config/i3-config-dasar3.js
@@ -10,7 +10,7 @@ SECURITY
 - saran dilakukan di awal, dan pastikan setting IP private sudah bisa di akses dilocal atau webfig
 - sistem user > add user > grup full > name: its.net > pass: pasmikro > lalu login ulang dg admin baru > kemudian disable yang bawaan   
 - system > identity > ITS.net >  
-- IP > services > ada list > disable dan sisakan 'www, winbox, ssh'
+- IP > services > ada list > disable dan sisakan 'www, winbox'
   - dklik u/ config ssh > available from isikan ip target 
     public dari DDNS misalnya,202.11.1.1 -lalu tambahkan IP local kita 192.168.50.0/24 lalu tambahkan ip statik spesifik 192.168.50.5
   - dclick winbox > available from isikan   192.168.50.0/24, lalu tambahkan  192.168.50.5 
