3a7813b6a1a2b9aa436887de41ebcf28b14c1c86
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-01-04 16:50:17 +0800

    Update i4-loginpage.js

--- KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i4-loginpage.js
+++ KODING/MIKROTIK/Aplikasikan/KONFIG-BISNIS/i4-loginpage.js
@@ -1,3 +1,4 @@
 -----------------------------------------------------------------------
 untuk mekanisme loginpage sudah kami buatkan petunjuk langkah dan teknis 
 silahkan merujuk ke dokumntasi dengan folder: loginpage/loginhotspot
+[my-directory](./my-directory)
