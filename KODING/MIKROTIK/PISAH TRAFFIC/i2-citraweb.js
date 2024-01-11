bf204133246857270d4aa77191e9caa9a8634bf7
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-01-11 16:51:47 +0800

    Update i2-citraweb.js

--- KODING/MIKROTIK/PISAH TRAFFIC/i2-citraweb.js
+++ KODING/MIKROTIK/PISAH TRAFFIC/i2-citraweb.js
@@ -2,7 +2,7 @@ Prioritas Bandwidth Video Conference Zoom - MIKROTIK TUTORIAL [ENG SUB]
 citraweb
 -------------------------------------------------------------------------------------------------
 kumpulkan 
-  adresslist,  ip, port, protocol
+  adresslist, ip, port, protocol
   addresslist bisa di daptkan update di https://midteknologi.com/blog/address-list-mikrotik/
 addresslist
   tambahkan address list
