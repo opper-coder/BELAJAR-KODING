d14cbf5e800947f939612b5adb109a024177e83b
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-01-23 09:07:44 +0800

    Update Extensi dan shortcut vscode.js

--- KODING/Extensi VS code/Extensi dan shortcut vscode.js
+++ KODING/Extensi VS code/Extensi dan shortcut vscode.js
@@ -40,8 +40,8 @@ angga rizki
 12. bulma snippet > ada banyak trigger misal hero, nav bar, dll menginspirasi
 13. auto rename tag (jun han) nulis tag dan edit auto penutup nya
 14. indent-rainbow
-16. console ninja
-17. codiumAi
+16. console ninja(VIP studio code) atau quokka(sandika)
+17. codiumAi, codeium
 ------------------------------------------------------
 1. cara menghapus element
 import yang tidak terpakai
