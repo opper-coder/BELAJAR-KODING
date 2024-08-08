c063deacdaed1222590c48097c45a08921853c14
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-08-08 21:41:37 +0800

    Update i2-topologi dan LB.js

--- KODING/MIKROTIK/A-BISNIS-2024/i2-topologi dan LB.js
+++ KODING/MIKROTIK/A-BISNIS-2024/i2-topologi dan LB.js
@@ -10,9 +10,22 @@ HOTSPOT
       - 4 > 4 > 8 
       - 4 > 4 > 2 > 4
       - 4 > 4 > 2 > 8
-      - masing2 SFP memiliki satu server hotspot 
+Ketentuan
+      - masing2 SFP memiliki satu server hotspot (loginpage masing2) 
       - semua SFP memiliki satu server hotspot 
-
-
+-----------------------------------------------------------
+TERMINAL ISP DHCP MIKROTIK
+      - dikirim HTB
+      - di terima RB750 second oleh 4 ether1234 > DHCP server /30 pada vlan1234 > 
+      - ether5 > bridgeLAN > porting semua vlan1234 
+      - kirim HTB gigabit
+-----------------------------------------------------------
+TERMINAL ISP pakai VLAN trunch pada SWITCH
+      - dikirim HTB
+      - diterima di switch di ether1234 bikin vlan lalu kirim ke trunch di ether5
+      - terima dari HTB ke mikrotik vlan tag pada mikrotik mode switch 
+      - atau terima pakai vlan pakai switch biasa
+-----------------------------------------------------------
+lalu jadikan load balance ECMP seperti repo PCC load balance LB ECMP
 
 -----------------------------------------------------------
