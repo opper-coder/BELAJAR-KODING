ed68d3f844a5758d11974ae8a8c7e55e10c3a9e4
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-08-08 21:25:06 +0800

    Update i2-topologi dan LB.js

--- KODING/MIKROTIK/A-BISNIS-2024/i2-topologi dan LB.js
+++ KODING/MIKROTIK/A-BISNIS-2024/i2-topologi dan LB.js
@@ -1,6 +1,18 @@
 -----------------------------------------------------------
-TOPOLOGI dan Loadbalance
-      topologi distribusi: ODN > ODC > ODP > ONU
+TOPOLOGI dan LOAD BALANCE
+      topologi distribusi: ODN > ODC > ODP > ONT
       topologi ISP: SWITCH, VLAN, MIKROTIK
       ECMP
+-----------------------------------------------------------
+HOTSPOT
+      - SFP 8db > 5:95 > 10:90 > 50:50 > ODC > ODP > ONT
+      - SFP 9db > 3:97 >  5:95 > 50:50 > ODC > ODP > ONT
+      - 4 > 4 > 8 
+      - 4 > 4 > 2 > 4
+      - 4 > 4 > 2 > 8
+      - masing2 SFP memiliki satu server hotspot 
+      - semua SFP memiliki satu server hotspot 
+
+
+
 -----------------------------------------------------------
