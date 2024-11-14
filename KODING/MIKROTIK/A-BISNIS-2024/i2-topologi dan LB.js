8d6541675546913f346dbd1f3584220172938cb4
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-11-14 08:17:50 +0700

    Update i2-topologi dan LB.js

--- KODING/MIKROTIK/A-BISNIS-2024/i2-topologi dan LB.js
+++ KODING/MIKROTIK/A-BISNIS-2024/i2-topologi dan LB.js
@@ -4,6 +4,17 @@ TOPOLOGI dan LOAD BALANCE
       topologi ISP: SWITCH, VLAN, MIKROTIK
       ECMP
 -----------------------------------------------------------
+IP ADDRESS DESIGN
+bridge1-WAN				= auto
+	vlan40-HS0-LOC		= 192.168.40.1/24       : hotspot vlan
+	vlan60-PP0-LOC		= 192.168.60.1/24       : pppoe vlan 
+bridge2-HOTSPOT			= 192.168.80.1/24       : hotspot bridge
+bridge3-PPPOE			= 192.168.100.1/24      : pppoe bridge
+vlan500-ISPBalang 		= 192.168.222.1/24 	: vlan500+ (ISP dimulai dari 500)
+bridge4-RUMAH			= 192.168.5.1/28		: 0-15/1-14
+bridge5-LB				= 222.222.222.1/30 	: loadbalance
+
+-----------------------------------------------------------
 HOTSPOT
       - SFP 8db > 5:95 > 10:90 > 50:50 > ODC > ODP > ONT     = +8 -> 5:10:50
       - SFP 9db > 3:97 >  5:95 > 50:50 > ODC > ODP > ONT     = +9 -> 3: 5:50
