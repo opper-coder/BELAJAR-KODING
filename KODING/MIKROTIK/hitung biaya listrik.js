9589330251108a2416a2485c3e253f31119e113a
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-12-25 16:57:28 +0700

    Update hitung biaya listrik.js

--- KODING/MIKROTIK/hitung biaya listrik.js
+++ KODING/MIKROTIK/hitung biaya listrik.js
@@ -6,3 +6,23 @@ perkiraan hitungan router per bulan pada meteran 450w r1
 
 12v x 2amp x 24jam  x 30 hari = 17.280 whatt
 17.280  x Rp.495.412844037 = sekitar Rp 8.500
+
+KABEL LAN POE 
+---------------------------------------------------
+ujung kanan biasa
+	putih oren 	: 1 
+	oren 		: 2 
+	putih hijau 	: 3 
+	biru 		: 4 
+	putih biru 	: 5 
+	Hijau		: 6 
+	putih coklat	: 7 
+	coklat 		: 8 
+kutub kiri
+	putih oren 	: 1 
+	oren 		: 2 
+	putih hijau 	: 3 
+	hijau 		: 6 
+	--
+	biru 		: (+) 
+	coklat 		: (-)
