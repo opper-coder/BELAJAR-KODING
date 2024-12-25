619365cec865185243c07bbfe41cf9e4228c535a
opper-coder <80605570+opper-coder@users.noreply.github.com>
2024-12-25 16:46:51 +0700

    Update i1-mikrotik.js

--- KODING/MIKROTIK/A-BISNIS-2024/i1-mikrotik.js
+++ KODING/MIKROTIK/A-BISNIS-2024/i1-mikrotik.js
@@ -5,33 +5,29 @@ BASIC CONFIG
       ECMP
 ------------------------------------------------------------------------------
 1. Reset config
+	a. system user grup=full
+	b. system identity
+	c. ip service
+	d. tool RoMON: pasmikromon
+	e. loginpage: edit HTML pada script input diatas password=username, matikan kolom password UI 
 2. Bridge: 
 	- bridge1-WAN 
-	- bridge2-HOTSPOT
-		- VLAN200-HS: (VLAN200-299) 
-			- tiap SFP: berisi 60 rumah dan satu ODN buat satu server-hotspot
-			- 1100ahx4: kapasitas 600 orang: sekitar 10 vlan
-	- bridge3-PPPoE
-		- VLAN100-PP (100-199)
-			- PPPoE: remote dan register semua PPPoE
-			- tiap wilayah boleh bikin pppoe lagi (pertimbangkan)
+	- bridge2-LAN
 	- bridge3-Remote
-		- VLAN10-remote: OLT, ONU, IPTV, Listrik, Pfsense, LB
-2.1. bikin VLAN
+3. VLAN
 	- hotspot, pppoe, remote
-3. porting Bridge - ether dan VLAN 
-4. IP bridge-WAN (DHCP client-bound, satu ether saja menuju ke Mikrotik LB, atau langsung ISP nanti kalau LB tinggal renew saja)
-5. IP bridge-HOTSPOT > VLAN200-HS 
-	- 192.168.40.1/22 (hotspot. 	interface: bridge-LAN)  pool 100 akan di binding (karena hotspot per sfp pilih /24(256) pas satu segment)
-	- 192.168.60.1/24 (pppoe. 	interface: VLAN60-PPPoE) dibuat saat bikin secret, tidak dibuat pada interface)
-	- 192.168.80.1/24 (remote OLT. 	interface: VLAN80-OLT)
-	- pada tutorial di global hotspot juga menggunakan VLAN, jadi tidak langsung di bridge
+4. porting
+	- ether, vlan
+5. IP	
+	- lihat topology
 6. DNS
-	- 8.8.8.8, 8.8.4.4, (ip gateway ISP jg boleh) allow remote ok
+	- 8.8.8.8, 8.8.4.4, 1.1.1.1 (ip gateway ISP jg boleh) allow remote=y
 7. NAT masquerade
 	- chain: srcnat, out: WAN, act: masquerade
 -----------------------------------------------------------------------------------
-8. HOTSPOT
+8. IP bridge-WAN (DHCP client-bound, satu ether saja menuju ke Mikrotik LB, atau langsung ISP nanti kalau LB tinggal renew saja)
+-----------------------------------------------------------------------------------
+9. HOTSPOT
 	- IP > HOTSPOT > Interface
 		- interface: konsepnya
 			- tiap SFP satu hotspot segment /24
@@ -58,18 +54,12 @@ BASIC CONFIG
 		- profil dan user yang di buat NATIVE jangan sampai di EDIT atau bahkan sekedar di buka di MIKBOTAM dst. 
 		  bisa ada masalah(logout sendiri dan anomali, ada intervensi dari MIKBOTAM dan sejenisnya)
 -----------------------------------------------------------------------------------
-9. PPPoE server
+10. PPPoE server
 	- add server. interface: VLAN tujuan
 	- add pppoe profile, limit, only one, local addr(gateway), remote addr(pool) bisa di buat disini jika kita memiliki ip pool xx - xx
 	- add secret: name, pass, IP PPPoE(gateway), IP perangkat(static) jika maunya statik, jika dinamik pakai pool pada profile saja  
 	- bisa di buat di mikbotam aja
 -----------------------------------------------------------------------------------
-10. Security: 
-	- user: administrator1212 grup full
-	- pass: passmikro
-	- identity: Surya-Hotspot
-	- services: api:8074, winbox:7273, (bukan default) (ini hanya saya simpan saja siapa tahu ada gunanya port API VPN : 6263)
-	- matikan semua service kecuali: winbox dan api
 11. CLOCK 
 	system > clock
 	> system zone auto detect: matikan
@@ -94,11 +84,7 @@ BASIC CONFIG
 			hidupkan zone auto detect:
 				tab manual time zone
 					WITA +8 
-12. pisah traffic
-	- WA
-	- freefire, mobile legend (caritahu dengan monitoring aktifitas)
-	- dengan ratio 1:4 Up:Do sudah tidak perlu lagi pisah traffic
-13. no share again 
+12. no share again 
 	+ batasi share kembali ke perangkat lain
 	- berdampak pada repeter, HTB estafet, wisp dll (cekidot)
 	ip > firewall > mangle 
@@ -108,19 +94,18 @@ BASIC CONFIG
 		new TTL: 1 (boolean)
 		passtrhrough: false (citraweb), true (channel lain)
 		Apply
-14. Restart 30 hari sekali terminal
+13. Restart 30 hari sekali terminal
 	/system script
 	add name=restart-router policy=ftp,reboot,read,write,policy,test,password,sniff,sensitive source="/system reboot"
 
 	/system scheduler
 	add name=restart-scheduler interval=30d start-time=04:00:00 on-event=restart-router
 
-	-- atau ini saja
+	----- atau ini saja
 	/system scheduler
 	add name=restart_router start-time=04:00:00 interval=1d on-event="/system reboot"
 
-
-15. AUTO RECONNECT VPN DDNS
+14. AUTO RECONNECT VPN DDNS
 ------------------------------------------------------------------------------------------------------ 
 - :tools > netwatch > add 
 	tab HOST
@@ -133,7 +118,7 @@ BASIC CONFIG
 		/interface disable [nama INTERFACE] (misal:  panda@mytunnel.id, IP VPN CHR cloud kita)
 		/interface enable [nama INTERFACE] (misal:  panda@mytunnel.id, IP VPN CHR cloud kita)
 
-16. DHCP ROGUE
+15. DHCP ROGUE
 -------------------
 adalah ada DHCP tandingan pd jaringan local kita 
 hal ini bisa membuat Connect tidak dapat internet, 
