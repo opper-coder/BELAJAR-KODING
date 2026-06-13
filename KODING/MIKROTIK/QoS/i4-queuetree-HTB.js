1408faa41c39654130e3a4569f2f6c613ca86799
opper-coder <80605570+opper-coder@users.noreply.github.com>
2026-06-13 20:18:54 +0800

    Update i4-queuetree-HTB.js

--- KODING/MIKROTIK/QoS/i4-queuetree-HTB.js
+++ KODING/MIKROTIK/QoS/i4-queuetree-HTB.js
@@ -1,17 +1,17 @@
 /* DAFTAR ISI
 ---------------------------------------------------------------------------------------------------
-- konsep 			-> konfig dasar (kombinasi paket mark dan HTB)
-- packet mark 		-> disinilah paket di buat
+- konsep 			-> konfig dasar (kombinasi packet-mark dan HTB)
+- packet mark 		-> membuat penanda mangle packet-mark
 - HTB queue tree 	-> implementasi
 - HTB 				-> hirarki prioritas (HTB inilah yang jadi perhatian dalam queue tree)
 
 QUEUE TREE DAN HTB
 ---------------------------------------------------------------------------------------------------
-queue tree adalah methode manajemen bandwidth dengan pola hirarchi HTB. issyu umum 
+queue tree adalah methode manajemen bandwidth dengan pola hirarchi HTB. isyu umum 
 	- queue independen 	: setiap queue bisa di config secara mandiri
 	- priority 			: jika di kombinasikan akan ada prioritas
 	- parent child		: basic hirarki
-	- HTB 				: pola susunan otoritas
+	- HTB 				: pola susunan otoritas dan prioritas
 	simple queue ataupun qtree adalah implementasi HTB 
 	HTB = hirarkikal token bucket adalah limitasi hirarkikal tumpukan
 	- berguna 'hirarkikal' untuk divisi2 di kantor dsb jadi bukan hanya limitasi per user tapi juga per grup
@@ -40,23 +40,13 @@ HTB adlah implementasi hirarki orang tua, anak, cucu dst untuk mengatur priority
 
 1. cara bikin paket mark:
 	firewall > mangle > add > 
-		tab general >
-			chain: prerouting
-			src address: IP client, protocol, interface, port, tcp, dll (disinilah setting untuk prioritas game dll)
-		tab action >
-			action: mark connection
-			new connection mark: (beri nama) conn-client1
-			passthrought: true
+		tab general > chain=prerouting src-address=IP client, protocol, interface, port, tcp, dll (disinilah setting untuk prioritas game dll)
+		tab action > action=mark-connection new-connection-mark="conn-client1" passthrought=true
 
 2. cara tangkap mark connection di atas
 	firewall > mangle > add > 
-		tab general >
-			chain: prerouting
-			connection mark: conn-client1
-		tab action >
-			action: mark packet
-			new connection mark: (beri nama2) packet-client1
-			passthrought: false
+		tab general > chain=prerouting connection-mark=Pilih conn-client1
+		tab action > action=mark-packet new-connection-mark=(beri nama2) "packet-client1" passthrought=false
 
 3. saat di apply maka akan ada trafficnya di field bytes nya
 
@@ -64,26 +54,26 @@ HTB adlah implementasi hirarki orang tua, anak, cucu dst untuk mengatur priority
 	queue > tab queue tree > add 
 		tab general >
 			name: queue1-upload 		// nama(terserah) 
-			parent: bridge-WAN 		// jalur internet (di ubah ke parent, nantinya)
+			parent: bridge-WAN 			// jalur internet (di ubah ke parent, nantinya)
 			packet mark: packet-cliet1 	// (pilih yg sudah di buat) 
-			queue type: pilih 		// ada pembahasan sendiri
-			max limit: 10M    		// max limit untuk upload
+			queue type: pilih 			// ada pembahasan sendiri
+			max limit: 10M    			// max limit untuk upload
 		apply > OK : akan terlihat ada traffic pada field avg. rate 
 
 5. implementasi HTB. yaitu bikin dulu queue yang jadi parent 
 	queue > tab queue tree > add 
 		tab general 
 			name: queue1-upload-parent 	// nama parent(terserah) 
-			parent: bridge-WAN 		// jalur internet
-			packet mark: 			// kosongkan (yg punya packet adalah childnya)
-			queue type: pilih 		// sesuaikan saja
-			max limit: 20M    		// max limit untuk upload parent
+			parent: bridge-WAN 			// jalur internet
+			packet mark: 				// kosongkan (yg punya packet adalah childnya)
+			queue type: pilih 			// sesuaikan saja
+			max limit: 20M    			// max limit untuk upload parent
 		apply > OK : akan terlihat ada traffic pada field avg. rate 
 
 6. masukan hirarki parent child
 	pada perent di child ubah jadi queue-uopload-parent
-		parent: queue1-upload-parent		// ubahan dari bridge-WAN (ganti parent)
-		apply > OK 				// akan terlihat hirarkinya
+		parent: queue1-upload-parent	// ubahan dari bridge-WAN (ganti parent)
+		apply > OK 						// akan terlihat hirarkinya
 
 */
 
@@ -92,8 +82,8 @@ HTB adlah implementasi hirarki orang tua, anak, cucu dst untuk mengatur priority
 HTB bisa juga pada simple queue langkah2 nya
 - bikin child child nya dulu
 	queue > tab simple queue > tab general
-		name: queue1-child 			// nama terserah
-		target: 10.10.10.1 			// client
+		name: queue1-child 				// nama terserah
+		target: 10.10.10.1 				// client
 		max limit : Ul 5M Dl 5M  
 - bikin parent:
 	queue > tab simple queue > tab general
@@ -141,16 +131,16 @@ queue-parent-A: 10M 				// total limit at yang di keluarkan parent 10
 	queue-child-C: 10M / 5M			// saat keduanya mengakses maka hanya kebagian B: 5M dan C: 5M 
 								   dan tidak ada sisa karena total A 10M
 skenario 2: child tetap mendapatkan limit at penuh meski melampaui limit parent. prioritas limit at pada child
-	A: 5M 					// limit parent lebih kecil dari limit child maka yang di dahulukan limit child
-		B: 5M/10M 			// dapat 5M atau <5 (kalau >5 tidak bisa karena sudah melebihi limit at parent)
-		C: 5M/10M 			// dapat 5M juga total 10M padahal parent cuma 5M
+	A: 5M 							// limit parent lebih kecil dari limit child maka yang di dahulukan limit child
+		B: 5M/10M 					// dapat 5M atau <5 (kalau >5 tidak bisa karena sudah melebihi limit at parent)
+		C: 5M/10M 					// dapat 5M juga total 10M padahal parent cuma 5M
 	kita seakan akan kebocoran 5M (ini sebuah kesalahan settingan) 
 
 skenario 3: di kasih priority limit at dulu, sisanya di bagi sesuai priority sebesar limit at
-	A: 16 					// limit parent 16M
-		B: 5M/10M p:8			// dapat limit at 5M 
-		C: 5M/10M p:1			// dapat limit at 5M sisa 6M dimbil sebesar limit at 5M di C dulu karena priority 1 (10M)
-						// sedang B mendapat tambahan 1M karena belakangan jadi 5+1 = 6M
+	A: 16 							// limit parent 16M
+		B: 5M/10M p:8				// dapat limit at 5M 
+		C: 5M/10M p:1				// dapat limit at 5M sisa 6M dimbil sebesar limit at 5M di C dulu karena priority 1 (10M)
+									// sedang B mendapat tambahan 1M karena belakangan jadi 5+1 = 6M
 skenario 4: jika priority sama (misalnya 8 semua) maka akan di bagi rata sama kayak tidak pakai priority (sekenario 1)
 
 */
